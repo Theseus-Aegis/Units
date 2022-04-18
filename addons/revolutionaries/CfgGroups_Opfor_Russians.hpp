d403b6eef926409959400338538f4782724ce18e
Lairdd1989 <54322466+Lairdd1989@users.noreply.github.com>
2022-04-18 10:00:57 +0100

    Unit Groups
    Adds groups for vet units

--- addons/revolutionaries/CfgGroups_Opfor_Russians.hpp
+++ addons/revolutionaries/CfgGroups_Opfor_Russians.hpp
@@ -269,3 +269,171 @@ class TACU_Revolutionaries_G_O_Infantry_Russians {
         };
     };
 };
+
+// Russian Revolutionaries Vets
+class TACU_Revolutionaries_G_O_Veterans_Russians {
+    name = "Veterans (Russians)";
+
+    class TACU_Revolutionaries_G_O_Veterans_Russians_Patrol {
+        name = "Patrol";
+        side = 0;
+        faction = "TACU_Revolutionaries_O";
+        icon = "\a3\ui_f\data\map\markers\nato\o_inf.paa";
+        class unit0 {
+            vehicle = "TACU_Revolutionaries_U_O_Russian_Vet_Rifleman01";
+            side = 0;
+            rank = "SERGEANT";
+            position[] = {0, 0, 0};
+        };
+        class unit1 {
+            vehicle = "TACU_Revolutionaries_U_O_Russian_Vet_Rifleman02";
+            side = 0;
+            rank = "PRIVATE";
+            position[] = {5, -5, 0};
+        };
+    };
+
+    class TACU_Revolutionaries_G_O_Veterans_Russians_Scout {
+        name = "Scouts";
+        side = 0;
+        faction = "TACU_Revolutionaries_O";
+        icon = "\a3\ui_f\data\map\markers\nato\o_inf.paa";
+        class unit0 {
+            vehicle = "TACU_Revolutionaries_U_O_Russian_Vet_SMG01";
+            side = 0;
+            rank = "SERGEANT";
+            position[] = {0, 0, 0};
+        };
+        class unit1 {
+            vehicle = "TACU_Revolutionaries_U_O_Russian_Vet_SMG02";
+            side = 0;
+            rank = "PRIVATE";
+            position[] = {5, -5, 0};
+        };
+    };
+
+    class TACU_Revolutionaries_G_O_Veterans_Russians_SniperTeam {
+        name = "Sniper Team";
+        side = 0;
+        faction = "TACU_Revolutionaries_O";
+        icon = "\a3\ui_f\data\map\markers\nato\o_inf.paa";
+        class unit0 {
+            vehicle = "TACU_Revolutionaries_U_O_Russian_Vet_Marksman";
+            side = 0;
+            rank = "SERGEANT";
+            position[] = {0, 0, 0};
+        };
+        class unit1 {
+            vehicle = "TACU_Revolutionaries_U_O_Russian_Vet_Rifleman04";
+            side = 0;
+            rank = "PRIVATE";
+            position[] = {5, -5, 0};
+        };
+    };
+
+    class TACU_Revolutionaries_G_O_Veterans_Russians_Fireteam {
+        name = "Fireteam";
+        side = 0;
+        faction = "TACU_Revolutionaries_O";
+        icon = "\a3\ui_f\data\map\markers\nato\o_inf.paa";
+        class unit0 {
+            vehicle = "TACU_Revolutionaries_U_O_Russian_Vet_Leader";
+            side = 0;
+            rank = "SERGEANT";
+            position[] = {0, 0, 0};
+        };
+        class unit1 {
+            vehicle = "TACU_Revolutionaries_U_O_Russian_Vet_Medic";
+            side = 0;
+            rank = "PRIVATE";
+            position[] = {5, -5, 0};
+        };
+        class unit2 {
+            vehicle = "TACU_Revolutionaries_U_O_Russian_Vet_Rifleman03";
+            side = 0;
+            rank = "PRIVATE";
+            position[] = {-5, -5, 0};
+        };
+        class unit3 {
+            vehicle = "TACU_Revolutionaries_U_O_Russian_Vet_Autorifleman";
+            side = 0;
+            rank = "PRIVATE";
+            position[] = {10, -10, 0};
+        };
+    };
+
+    class TACU_Revolutionaries_G_O_Veterans_Russians_Squad {
+        name = "Squad (Heavy)";
+        side = 0;
+        faction = "TACU_Revolutionaries_O";
+        icon = "\a3\ui_f\data\map\markers\nato\o_inf.paa";
+        class unit0 {
+            vehicle = "TACU_Revolutionaries_U_O_Russian_Vet_Leader";
+            side = 0;
+            rank = "SERGEANT";
+            position[] = {0, 0, 0};
+        };
+        class unit1 {
+            vehicle = "TACU_Revolutionaries_U_O_Russian_Vet_Rifleman02";
+            side = 0;
+            rank = "PRIVATE";
+            position[] = {5, -5, 0};
+        };
+        class unit2 {
+            vehicle = "TACU_Revolutionaries_U_O_Russian_Vet_Rifleman03";
+            side = 0;
+            rank = "PRIVATE";
+            position[] = {-5, -5, 0};
+        };
+        class unit3 {
+            vehicle = "TACU_Revolutionaries_U_O_Russian_Vet_Rifleman04";
+            side = 0;
+            rank = "PRIVATE";
+            position[] = {10, -10, 0};
+        };
+        class unit4 {
+            vehicle = "TACU_Revolutionaries_U_O_Russian_Vet_Autorifleman";
+            side = 0;
+            rank = "PRIVATE";
+            position[] = {-10, -10, 0};
+        };
+        class unit5 {
+            vehicle = "TACU_Revolutionaries_U_O_Russian_Vet_Medic";
+            side = 0;
+            rank = "PRIVATE";
+            position[] = {15, -15, 0};
+        };
+    };
+
+    class TACU_Revolutionaries_G_O_Veterans_Russians_Support {
+        name = "Support";
+        side = 0;
+        faction = "TACU_Revolutionaries_O";
+        icon = "\a3\ui_f\data\map\markers\nato\o_inf.paa";
+        class unit0 {
+            vehicle = "TACU_Revolutionaries_U_O_Russian_Vet_Shotgunner";
+            side = 0;
+            rank = "SERGEANT";
+            position[] = {0, 0, 0};
+        };
+        class unit1 {
+            vehicle = "TACU_Revolutionaries_U_O_Russian_Vet_Machinegunner";
+            side = 0;
+            rank = "PRIVATE";
+            position[] = {5, -5, 0};
+        };
+        class unit2 {
+            vehicle = "TACU_Revolutionaries_U_O_Russian_Vet_Rocketeer";
+            side = 0;
+            rank = "PRIVATE";
+            position[] = {-5, -5, 0};
+        };
+        class unit3 {
+            vehicle = "TACU_Revolutionaries_U_O_Russian_Vet_Rifleman01";
+            side = 0;
+            rank = "PRIVATE";
+            position[] = {10, -10, 0};
+        };
+    };
+
+};
