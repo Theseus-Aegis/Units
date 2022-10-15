c2ed6b9f3053e29a16df8b044f425bbe9d974e4c
TyroneMF <TyroneMF@hotmail.com>
2022-10-16 00:33:29 +0100

    Add custom uniforms

---
+++ addons/cartel/CfgVehicles_Uniforms.hpp
@@ -0,0 +1,37 @@
+class C_Uniform_ArtTShirt_01_v1_F;
+class TACU_Uniform_O_V_Cartel_01: C_Uniform_ArtTShirt_01_v1_F {
+    scope = 1;
+    author = "GilleeDoo, Mike";
+    uniformClass = "TACU_Uniform_O_W_Cartel_01";
+    hiddenSelectionsTextures[] = {QPATHTOF(data\tacu_uniform_cartel_01_co.paa)};
+};
+class TACU_Uniform_O_V_Cartel_02: C_Uniform_ArtTShirt_01_v1_F {
+    scope = 1;
+    author = "GilleeDoo, Mike";
+    uniformClass = "TACU_Uniform_O_W_Cartel_02";
+    hiddenSelectionsTextures[] = {QPATHTOF(data\tacu_uniform_cartel_02_co.paa)};
+};
+class TACU_Uniform_O_V_Cartel_03: C_Uniform_ArtTShirt_01_v1_F {
+    scope = 1;
+    author = "GilleeDoo, Mike";
+    uniformClass = "TACU_Uniform_O_W_Cartel_03";
+    hiddenSelectionsTextures[] = {QPATHTOF(data\tacu_uniform_cartel_03_co.paa)};
+};
+class TACU_Uniform_O_V_Cartel_04: C_Uniform_ArtTShirt_01_v1_F {
+    scope = 1;
+    author = "GilleeDoo, Mike";
+    uniformClass = "TACU_Uniform_O_W_Cartel_04";
+    hiddenSelectionsTextures[] = {QPATHTOF(data\tacu_uniform_cartel_04_co.paa)};
+};
+class TACU_Uniform_O_V_Cartel_05: C_Uniform_ArtTShirt_01_v1_F {
+    scope = 1;
+    author = "GilleeDoo, Mike";
+    uniformClass = "TACU_Uniform_O_W_Cartel_05";
+    hiddenSelectionsTextures[] = {QPATHTOF(data\tacu_uniform_cartel_05_co.paa)};
+};
+class TACU_Uniform_O_V_Cartel_06: C_Uniform_ArtTShirt_01_v1_F {
+    scope = 1;
+    author = "GilleeDoo, Mike";
+    uniformClass = "TACU_Uniform_O_W_Cartel_06";
+    hiddenSelectionsTextures[] = {QPATHTOF(data\tacu_uniform_cartel_06_co.paa)};
+};
---
+++ addons/cartel/CfgWeapons.hpp
@@ -0,0 +1,50 @@
+class CfgWeapons {
+    // Uniforms
+    class Uniform_Base;
+    class U_C_ArtTShirt_01_v1_F: Uniform_Base {
+        class ItemInfo;
+    };
+
+    class TACU_Uniform_O_W_Cartel_01: U_C_ArtTShirt_01_v1_F {
+        displayName = "Cartel Uniform 01";
+        scope = 1;
+        class ItemInfo: ItemInfo {
+            uniformClass = "TACU_Uniform_O_V_Cartel_01";
+        };
+    };
+    class TACU_Uniform_O_W_Cartel_02: U_C_ArtTShirt_01_v1_F {
+        displayName = "Cartel Uniform 02";
+        scope = 1;
+        class ItemInfo: ItemInfo {
+            uniformClass = "TACU_Uniform_O_V_Cartel_02";
+        };
+    };
+    class TACU_Uniform_O_W_Cartel_03: U_C_ArtTShirt_01_v1_F {
+        displayName = "Cartel Uniform 03";
+        scope = 1;
+        class ItemInfo: ItemInfo {
+            uniformClass = "TACU_Uniform_O_V_Cartel_03";
+        };
+    };
+    class TACU_Uniform_O_W_Cartel_04: U_C_ArtTShirt_01_v1_F {
+        displayName = "Cartel Uniform 04";
+        scope = 1;
+        class ItemInfo: ItemInfo {
+            uniformClass = "TACU_Uniform_O_V_Cartel_04";
+        };
+    };
+    class TACU_Uniform_O_W_Cartel_05: U_C_ArtTShirt_01_v1_F {
+        displayName = "Cartel Uniform 05";
+        scope = 1;
+        class ItemInfo: ItemInfo {
+            uniformClass = "TACU_Uniform_O_V_Cartel_05";
+        };
+    };
+    class TACU_Uniform_O_W_Cartel_06: U_C_ArtTShirt_01_v1_F {
+        displayName = "Cartel Uniform 06";
+        scope = 1;
+        class ItemInfo: ItemInfo {
+            uniformClass = "TACU_Uniform_O_V_Cartel_06";
+        };
+    };
+};
