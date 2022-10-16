c54e04383f5a6c73259c0c9594e600d6650f339b
TyroneMF <TyroneMF@hotmail.com>
2022-10-16 03:50:37 +0100

    Update grunt uniform path, add thug uniforms

--- addons/cartel/CfgVehicles_Uniforms.hpp
+++ addons/cartel/CfgVehicles_Uniforms.hpp
@@ -1,37 +1,77 @@
+// Grunt Uniforms
 class C_Uniform_ArtTShirt_01_v1_F;
-class TACU_Uniform_O_V_Cartel_01: C_Uniform_ArtTShirt_01_v1_F {
+class TACU_Uniform_O_V_Cartel_Grunt_01: C_Uniform_ArtTShirt_01_v1_F {
     scope = 1;
     author = "GilleeDoo, Mike";
-    uniformClass = "TACU_Uniform_O_W_Cartel_01";
-    hiddenSelectionsTextures[] = {QPATHTOF(data\tacu_uniform_cartel_01_co.paa)};
+    uniformClass = "TACU_Uniform_O_W_Cartel_Grunt_01";
+    hiddenSelectionsTextures[] = {QPATHTOF(data\tacu_uniform_cartel_grunt_01_co.paa)};
 };
-class TACU_Uniform_O_V_Cartel_02: C_Uniform_ArtTShirt_01_v1_F {
+class TACU_Uniform_O_V_Cartel_Grunt_02: C_Uniform_ArtTShirt_01_v1_F {
     scope = 1;
     author = "GilleeDoo, Mike";
-    uniformClass = "TACU_Uniform_O_W_Cartel_02";
-    hiddenSelectionsTextures[] = {QPATHTOF(data\tacu_uniform_cartel_02_co.paa)};
+    uniformClass = "TACU_Uniform_O_W_Cartel_Grunt_02";
+    hiddenSelectionsTextures[] = {QPATHTOF(data\tacu_uniform_cartel_grunt_02_co.paa)};
 };
-class TACU_Uniform_O_V_Cartel_03: C_Uniform_ArtTShirt_01_v1_F {
+class TACU_Uniform_O_V_Cartel_Grunt_03: C_Uniform_ArtTShirt_01_v1_F {
     scope = 1;
     author = "GilleeDoo, Mike";
-    uniformClass = "TACU_Uniform_O_W_Cartel_03";
-    hiddenSelectionsTextures[] = {QPATHTOF(data\tacu_uniform_cartel_03_co.paa)};
+    uniformClass = "TACU_Uniform_O_W_Cartel_Grunt_03";
+    hiddenSelectionsTextures[] = {QPATHTOF(data\tacu_uniform_cartel_grunt_03_co.paa)};
 };
-class TACU_Uniform_O_V_Cartel_04: C_Uniform_ArtTShirt_01_v1_F {
+class TACU_Uniform_O_V_Cartel_Grunt_04: C_Uniform_ArtTShirt_01_v1_F {
     scope = 1;
     author = "GilleeDoo, Mike";
-    uniformClass = "TACU_Uniform_O_W_Cartel_04";
-    hiddenSelectionsTextures[] = {QPATHTOF(data\tacu_uniform_cartel_04_co.paa)};
+    uniformClass = "TACU_Uniform_O_W_Cartel_Grunt_04";
+    hiddenSelectionsTextures[] = {QPATHTOF(data\tacu_uniform_cartel_grunt_04_co.paa)};
 };
-class TACU_Uniform_O_V_Cartel_05: C_Uniform_ArtTShirt_01_v1_F {
+class TACU_Uniform_O_V_Cartel_Grunt_05: C_Uniform_ArtTShirt_01_v1_F {
     scope = 1;
     author = "GilleeDoo, Mike";
-    uniformClass = "TACU_Uniform_O_W_Cartel_05";
-    hiddenSelectionsTextures[] = {QPATHTOF(data\tacu_uniform_cartel_05_co.paa)};
+    uniformClass = "TACU_Uniform_O_W_Cartel_Grunt_05";
+    hiddenSelectionsTextures[] = {QPATHTOF(data\tacu_uniform_cartel_grunt_05_co.paa)};
 };
-class TACU_Uniform_O_V_Cartel_06: C_Uniform_ArtTShirt_01_v1_F {
+class TACU_Uniform_O_V_Cartel_Grunt_06: C_Uniform_ArtTShirt_01_v1_F {
     scope = 1;
     author = "GilleeDoo, Mike";
-    uniformClass = "TACU_Uniform_O_W_Cartel_06";
-    hiddenSelectionsTextures[] = {QPATHTOF(data\tacu_uniform_cartel_06_co.paa)};
+    uniformClass = "TACU_Uniform_O_W_Cartel_Grunt_06";
+    hiddenSelectionsTextures[] = {QPATHTOF(data\tacu_uniform_cartel_grunt_06_co.paa)};
+};
+
+// Thug Uniforms
+class I_C_Soldier_Bandit_2_F;
+class TACU_Uniform_O_V_Cartel_Thug_01: I_C_Soldier_Bandit_2_F {
+    scope = 1;
+    author = "GilleeDoo, Mike";
+    uniformClass = "TACU_Uniform_O_W_Cartel_Thug_01";
+    hiddenSelectionsTextures[] = {QPATHTOF(data\tacu_uniform_cartel_thug_01_co.paa)};
+};
+class TACU_Uniform_O_V_Cartel_Thug_02: I_C_Soldier_Bandit_2_F {
+    scope = 1;
+    author = "GilleeDoo, Mike";
+    uniformClass = "TACU_Uniform_O_W_Cartel_Thug_02";
+    hiddenSelectionsTextures[] = {QPATHTOF(data\tacu_uniform_cartel_thug_02_co.paa)};
+};
+class TACU_Uniform_O_V_Cartel_Thug_03: I_C_Soldier_Bandit_2_F {
+    scope = 1;
+    author = "GilleeDoo, Mike";
+    uniformClass = "TACU_Uniform_O_W_Cartel_Thug_03";
+    hiddenSelectionsTextures[] = {QPATHTOF(data\tacu_uniform_cartel_thug_03_co.paa)};
+};
+class TACU_Uniform_O_V_Cartel_Thug_04: I_C_Soldier_Bandit_2_F {
+    scope = 1;
+    author = "GilleeDoo, Mike";
+    uniformClass = "TACU_Uniform_O_W_Cartel_Thug_04";
+    hiddenSelectionsTextures[] = {QPATHTOF(data\tacu_uniform_cartel_thug_04_co.paa)};
+};
+class TACU_Uniform_O_V_Cartel_Thug_05: I_C_Soldier_Bandit_2_F {
+    scope = 1;
+    author = "GilleeDoo, Mike";
+    uniformClass = "TACU_Uniform_O_W_Cartel_Thug_05";
+    hiddenSelectionsTextures[] = {QPATHTOF(data\tacu_uniform_cartel_thug_05_co.paa)};
+};
+class TACU_Uniform_O_V_Cartel_Thug_06: I_C_Soldier_Bandit_2_F {
+    scope = 1;
+    author = "GilleeDoo, Mike";
+    uniformClass = "TACU_Uniform_O_W_Cartel_Thug_06";
+    hiddenSelectionsTextures[] = {QPATHTOF(data\tacu_uniform_cartel_thug_06_co.paa)};
 };
--- addons/cartel/CfgWeapons.hpp
+++ addons/cartel/CfgWeapons.hpp
@@ -1,56 +1,103 @@
 class CfgWeapons {
-    // Uniforms
+    // Grunt Uniforms
     class Uniform_Base;
     class U_C_ArtTShirt_01_v1_F: Uniform_Base {
         class ItemInfo;
     };
 
-    class TACU_Uniform_O_W_Cartel_01: U_C_ArtTShirt_01_v1_F {
-        displayName = "Cartel Uniform 01";
+    class TACU_Uniform_O_W_Cartel_Grunt_01: U_C_ArtTShirt_01_v1_F {
+        displayName = "Cartel Grunt Uniform 01";
         scope = 1;
         class ItemInfo: ItemInfo {
-            uniformClass = "TACU_Uniform_O_V_Cartel_01";
+            uniformClass = "TACU_Uniform_O_V_Cartel_Grunt_01";
             containerClass = "Supply100";
         };
     };
-    class TACU_Uniform_O_W_Cartel_02: U_C_ArtTShirt_01_v1_F {
-        displayName = "Cartel Uniform 02";
+    class TACU_Uniform_O_W_Cartel_Grunt_02: U_C_ArtTShirt_01_v1_F {
+        displayName = "Cartel Grunt Uniform 02";
         scope = 1;
         class ItemInfo: ItemInfo {
-            uniformClass = "TACU_Uniform_O_V_Cartel_02";
+            uniformClass = "TACU_Uniform_O_V_Cartel_Grunt_02";
             containerClass = "Supply100";
         };
     };
-    class TACU_Uniform_O_W_Cartel_03: U_C_ArtTShirt_01_v1_F {
-        displayName = "Cartel Uniform 03";
+    class TACU_Uniform_O_W_Cartel_Grunt_03: U_C_ArtTShirt_01_v1_F {
+        displayName = "Cartel Grunt Uniform 03";
         scope = 1;
         class ItemInfo: ItemInfo {
-            uniformClass = "TACU_Uniform_O_V_Cartel_03";
+            uniformClass = "TACU_Uniform_O_V_Cartel_Grunt_03";
             containerClass = "Supply100";
         };
     };
-    class TACU_Uniform_O_W_Cartel_04: U_C_ArtTShirt_01_v1_F {
-        displayName = "Cartel Uniform 04";
+    class TACU_Uniform_O_W_Cartel_Grunt_04: U_C_ArtTShirt_01_v1_F {
+        displayName = "Cartel Grunt Uniform 04";
         scope = 1;
         class ItemInfo: ItemInfo {
-            uniformClass = "TACU_Uniform_O_V_Cartel_04";
+            uniformClass = "TACU_Uniform_O_V_Cartel_Grunt_04";
             containerClass = "Supply100";
         };
     };
-    class TACU_Uniform_O_W_Cartel_05: U_C_ArtTShirt_01_v1_F {
-        displayName = "Cartel Uniform 05";
+    class TACU_Uniform_O_W_Cartel_Grunt_05: U_C_ArtTShirt_01_v1_F {
+        displayName = "Cartel Grunt Uniform 05";
         scope = 1;
         class ItemInfo: ItemInfo {
-            uniformClass = "TACU_Uniform_O_V_Cartel_05";
+            uniformClass = "TACU_Uniform_O_V_Cartel_Grunt_05";
             containerClass = "Supply100";
         };
     };
-    class TACU_Uniform_O_W_Cartel_06: U_C_ArtTShirt_01_v1_F {
-        displayName = "Cartel Uniform 06";
+    class TACU_Uniform_O_W_Cartel_Grunt_06: U_C_ArtTShirt_01_v1_F {
+        displayName = "Cartel Grunt Uniform 06";
         scope = 1;
         class ItemInfo: ItemInfo {
-            uniformClass = "TACU_Uniform_O_V_Cartel_06";
+            uniformClass = "TACU_Uniform_O_V_Cartel_Grunt_06";
             containerClass = "Supply100";
+        };
+    };
+
+    // Thug Uniforms
+    class U_I_C_Soldier_Bandit_2_F: Uniform_Base {
+        class ItemInfo;
+    };
+    class TACU_Uniform_O_W_Cartel_Thug_01: U_I_C_Soldier_Bandit_2_F {
+        displayName = "Cartel Thug Uniform 01";
+        scope = 1;
+        class ItemInfo: ItemInfo {
+            uniformClass = "TACU_Uniform_O_V_Cartel_Thug_01";
+        };
+    };
+    class TACU_Uniform_O_W_Cartel_Thug_02: U_I_C_Soldier_Bandit_2_F {
+        displayName = "Cartel Thug Uniform 02";
+        scope = 1;
+        class ItemInfo: ItemInfo {
+            uniformClass = "TACU_Uniform_O_V_Cartel_Thug_02";
+        };
+    };
+    class TACU_Uniform_O_W_Cartel_Thug_03: U_I_C_Soldier_Bandit_2_F {
+        displayName = "Cartel Thug Uniform 03";
+        scope = 1;
+        class ItemInfo: ItemInfo {
+            uniformClass = "TACU_Uniform_O_V_Cartel_Thug_03";
+        };
+    };
+    class TACU_Uniform_O_W_Cartel_Thug_04: U_I_C_Soldier_Bandit_2_F {
+        displayName = "Cartel Thug Uniform 04";
+        scope = 1;
+        class ItemInfo: ItemInfo {
+            uniformClass = "TACU_Uniform_O_V_Cartel_Thug_04";
+        };
+    };
+    class TACU_Uniform_O_W_Cartel_Thug_05: U_I_C_Soldier_Bandit_2_F {
+        displayName = "Cartel Thug Uniform 05";
+        scope = 1;
+        class ItemInfo: ItemInfo {
+            uniformClass = "TACU_Uniform_O_V_Cartel_Thug_05";
+        };
+    };
+    class TACU_Uniform_O_W_Cartel_Thug_06: U_I_C_Soldier_Bandit_2_F {
+        displayName = "Cartel Thug Uniform 06";
+        scope = 1;
+        class ItemInfo: ItemInfo {
+            uniformClass = "TACU_Uniform_O_V_Cartel_Thug_06";
         };
     };
 };
