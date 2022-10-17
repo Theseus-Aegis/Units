29789d89e01544d526f68b44466232fae542299d
TyroneMF <TyroneMF@hotmail.com>
2022-10-17 15:58:37 +0100

    MSF - CfgGlasses

--- addons/msf/CfgGlasses.hpp
+++ addons/msf/CfgGlasses.hpp
@@ -31,19 +31,16 @@ class CfgGlasses {
         hiddenSelectionsMaterials[] = {QPATHTOF(data\scarves\msf_glasses_scarf.rvmat)};
     };
     class TACU_MSF_Glasses_Scarf_Khaki: TACU_MSF_Glasses_Scarf_Green {
-        author = "Facel";
         displayName = "Shoulder Scarf (Khaki)";
         picture = QPATHTOF(ui\scarves\msf_glasses_scarf_khaki_ca.paa);
         hiddenSelectionsTextures[] = {QPATHTOF(data\scarves\msf_glasses_scarf_khaki_co.paa)};
     };
     class TACU_MSF_Glasses_Scarf_Black: TACU_MSF_Glasses_Scarf_Green {
-        author = "Facel";
         displayName = "Shoulder Scarf (Black)";
         picture = QPATHTOF(ui\scarves\msf_glasses_scarf_black_ca.paa);
         hiddenSelectionsTextures[] = {QPATHTOF(data\scarves\msf_glasses_scarf_black_co.paa)};
     };
     class TACU_MSF_Glasses_Scarf_Red: TACU_MSF_Glasses_Scarf_Green {
-        author = "Facel";
         displayName = "Shoulder Scarf (Red)";
         picture = QPATHTOF(ui\scarves\msf_glasses_scarf_red_ca.paa);
         hiddenSelectionsTextures[] = {QPATHTOF(data\scarves\msf_glasses_scarf_red_co.paa)};
