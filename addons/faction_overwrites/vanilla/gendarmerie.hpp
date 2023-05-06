b2ebb7c5a7ef01ecca19fb7eaae4dfc8fda5db1e
Mike-MF <TyroneMF@hotmail.com>
2023-05-06 11:37:04 +0100

    Gendarmerie

---
+++ addons/faction_overwrites/vanilla/gendarmerie.hpp
@@ -0,0 +1,15 @@
+// Units - Gendarmerie
+class O_GEN_crew_lxWS: O_crew_F {
+    magazines[] = {mag_6("tacgt_AI_30Rnd_9x19_MP5"), mag_3("tacgt_AI_17Rnd_9x19_Walther"), "HandGrenade", "SmokeShell"};
+    respawnMagazines[] = {mag_6("tacgt_AI_30Rnd_9x19_MP5"), mag_3("tacgt_AI_17Rnd_9x19_Walther"), "HandGrenade", "SmokeShell"};
+};
+
+class B_GEN_Soldier_base_F: B_Soldier_F {
+    magazines[] = {mag_6("tacgt_AI_30Rnd_9x19_MP5"), mag_3("tacgt_AI_17Rnd_9x19_Walther"), "HandGrenade", "SmokeShell"};
+    respawnMagazines[] = {mag_6("tacgt_AI_30Rnd_9x19_MP5"), mag_3("tacgt_AI_17Rnd_9x19_Walther"), "HandGrenade", "SmokeShell"};
+};
+class B_GEN_Soldier_F: B_GEN_Soldier_base_F {};
+class B_GEN_Commander_F: B_GEN_Soldier_base_F {
+    magazines[] = {mag_6("tacgt_AI_30Rnd_9x19_MP5"), mag_3("tacgt_AI_17Rnd_9x19_Walther"), "HandGrenade", "SmokeShell"};
+    respawnMagazines[] = {mag_6("tacgt_AI_30Rnd_9x19_MP5"), mag_3("tacgt_AI_17Rnd_9x19_Walther"), "HandGrenade", "SmokeShell"};
+};
