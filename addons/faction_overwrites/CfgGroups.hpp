42b67448dd7977a658096fd9e36b557adb7bb452
Mike-MF <TyroneMF@hotmail.com>
2023-06-24 02:39:27 +0100

    Faction Overwrites - Remove CUP Groups (#72)

---
+++ addons/faction_overwrites/CfgGroups.hpp
@@ -0,0 +1,11 @@
+class CfgGroups {
+    class West {
+        #include "CfgGroups_West.hpp"
+    };
+    class East {
+        #include "CfgGroups_East.hpp"
+    };
+    class Indep {
+        #include "CfgGroups_Indep.hpp"
+    };
+};
