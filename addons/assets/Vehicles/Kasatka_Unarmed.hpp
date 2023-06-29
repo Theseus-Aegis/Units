f7b3398396e7e6771468785189af3e7146f0ed32
Mike-MF <TyroneMF@hotmail.com>
2023-06-29 04:15:59 +0100

    Add Kasatka base vehicles

---
+++ addons/assets/Vehicles/Kasatka_Unarmed.hpp
@@ -0,0 +1,49 @@
+// Ka-60 Kasatka Unarmed
+class O_Heli_Light_02_unarmed_F: Heli_Light_02_unarmed_base_F {
+    class EventHandlers;
+};
+
+class TACU_Kasatka_Unarmed_Base: O_Heli_Light_02_unarmed_F {
+    MACRO_CLEAR_VEHICLE_CARGO;
+    MACRO_DEFAULT_VEHICLE;
+    class TextureSources {
+        class Black {
+            author = "Bohemia Interactive";
+            displayName = "Black";
+            factions[] = {};
+            textures[] = {"\A3\Air_F\Heli_Light_02\Data\Heli_Light_02_ext_CO.paa"};
+        };
+        class BlackCustom {
+            author = "Bohemia Interactive";
+            displayName = "Black Custom";
+            factions[] = {};
+            textures[] = {"\A3\Air_F_Heli\Heli_Light_02\Data\Heli_Light_02_ext_OPFOR_V2_CO.paa"};
+        };
+        class Blue {
+            author = "Bohemia Interactive";
+            displayName = "Blue";
+            factions[] = {};
+            textures[] = {"\a3\air_f\Heli_Light_02\Data\heli_light_02_ext_civilian_co.paa"};
+        };
+        class Green {
+            author = "GilleeDoo";
+            displayName = "Green";
+            factions[] = {};
+            textures[] = {QPATHTOF(data\kasatka\heli_light_02_na_grn_ext_co.paa)};
+        };
+        class Yellow {
+            author = "GilleeDoo";
+            displayName = "Yellow";
+            factions[] = {};
+            textures[] = {QPATHTOF(data\kasatka\heli_light_02_na_ylw_ext_co.paa)};
+        };
+    };
+
+    textureList[] = {
+        "Black", 1,
+        "BlackCustom", 1,
+        "Blue", 1
+    };
+
+    MACRO_EVENTHANDLERS;
+};
