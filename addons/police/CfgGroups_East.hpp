cdd69175b9790fec3c031ed346c63f78c78b9768
Mike-MF <TyroneMF@hotmail.com>
2023-07-04 00:46:22 +0100

    Police

--- addons/police/CfgGroups_East.hpp
+++ addons/police/CfgGroups_East.hpp
@@ -8,41 +8,16 @@ class TACU_Police_O {
             side = 0;
             faction = "TACU_Police_O";
             icon = "\a3\ui_f\data\map\markers\nato\o_inf.paa";
-            class unit0 {
-                vehicle = "TACU_Police_U_O_CT_Rifleman";
-                side = 0;
-                rank = "SERGEANT";
-                position[] = {0, 0, 0};
-            };
-            class unit1: unit0 {
-                vehicle = "TACU_Police_U_O_CT_SMG_2";
-                side = 0;
-                rank = "PRIVATE";
-                position[] = {5, -5, 0};
-            };
+            MACRO_UNIT0(TACU_Police_U_O_CT_Rifleman,0);
+            MACRO_UNIT1(TACU_Police_U_O_CT_SMG_2,0);
         };
 
         class TACU_Police_G_O_CT_Fireteam: TACU_Police_G_O_CT_Guards {
             name = "Fireteam";
-            class unit0 {
-                vehicle = "TACU_Police_U_O_CT_Rifleman";
-                side = 0;
-                rank = "SERGEANT";
-                position[] = {0, 0, 0};
-            };
-            class unit1: unit0 {
-                vehicle = "TACU_Police_U_O_CT_Breacher";
-                rank = "PRIVATE";
-                position[] = {5, -5, 0};
-            };
-            class unit2: unit1 {
-                vehicle = "TACU_Police_U_O_CT_SMG_1";
-                position[] = {-5, -5, 0};
-            };
-            class unit3: unit2 {
-                vehicle = "TACU_Police_U_O_CT_SMG_2";
-                position[] = {10, -10, 0};
-            };
+            MACRO_UNIT0(TACU_Police_U_O_CT_Rifleman,0);
+            MACRO_UNIT1(TACU_Police_U_O_CT_Breacher,0);
+            MACRO_UNIT2(TACU_Police_U_O_CT_SMG_1,0);
+            MACRO_UNIT3(TACU_Police_U_O_CT_SMG_2,0);
         };
     };
 
@@ -54,17 +29,8 @@ class TACU_Police_O {
             side = 0;
             faction = "TACU_Police_O";
             icon = "\a3\ui_f\data\map\markers\nato\o_inf.paa";
-            class unit0 {
-                vehicle = "TACU_Police_U_O_Enforcer_SMG_1";
-                side = 0;
-                rank = "SERGEANT";
-                position[] = {0, 0, 0};
-            };
-            class unit1: unit0 {
-                vehicle = "TACU_Police_U_O_Enforcer_SMG_2";
-                rank = "PRIVATE";
-                position[] = {5, -5, 0};
-            };
+            MACRO_UNIT0(TACU_Police_U_O_Enforcer_SMG_1,0);
+            MACRO_UNIT1(TACU_Police_U_O_Enforcer_SMG_2,0);
         };
 
         class TACU_Police_G_O_Enforcer_Fireteam: TACU_Police_G_O_Enforcer_Guards {
@@ -72,25 +38,10 @@ class TACU_Police_O {
             side = 0;
             faction = "TACU_Police_O";
             icon = "\a3\ui_f\data\map\markers\nato\o_inf.paa";
-            class unit0 {
-                vehicle = "TACU_Police_U_O_Enforcer_SMG_1";
-                side = 0;
-                rank = "SERGEANT";
-                position[] = {0, 0, 0};
-            };
-            class unit1: unit0 {
-                vehicle = "TACU_Police_U_O_Enforcer_Breacher";
-                rank = "PRIVATE";
-                position[] = {5, -5, 0};
-            };
-            class unit2: unit1 {
-                vehicle = "TACU_Police_U_O_Enforcer_SMG_2";
-                position[] = {-5, -5, 0};
-            };
-            class unit3: unit2 {
-                vehicle = "TACU_Police_U_O_Enforcer_SMG_3";
-                position[] = {10, -10, 0};
-            };
+            MACRO_UNIT0(TACU_Police_U_O_Enforcer_SMG_1,0);
+            MACRO_UNIT1(TACU_Police_U_O_Enforcer_Breacher,0);
+            MACRO_UNIT2(TACU_Police_U_O_Enforcer_SMG_2,0);
+            MACRO_UNIT3(TACU_Police_U_O_Enforcer_SMG_3,0);
         };
     };
 };
