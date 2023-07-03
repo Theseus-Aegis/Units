2e8d4cd54facfa97cd02986f3f57eec0c0798b2a
Mike-MF <TyroneMF@hotmail.com>
2023-07-04 00:02:15 +0100

    Add Turrets

--- addons/taliban/CfgVehicles.hpp
+++ addons/taliban/CfgVehicles.hpp
@@ -6,11 +6,14 @@ class CfgVehicles {
     // Militia
     #include "Militia\CfgVehicles_Militia_I.hpp"
     #include "Militia\CfgVehicles_Militia_O.hpp"
+    #include "Militia\CfgVehicles_Turrets_Militia.hpp"
     #include "Militia\CfgVehicles_Vehicles_Militia.hpp"
 
     // Veterans
     #include "Veterans\CfgVehicles_Veterans_I.hpp"
     #include "Veterans\CfgVehicles_Veterans_O.hpp"
+    #include "Veterans\CfgVehicles_Turrets_Veterans.hpp"
     #include "Veterans\CfgVehicles_Vehicles_Veterans.hpp"
+
     #include "CfgVehicles_Backpacks.hpp"
 };
