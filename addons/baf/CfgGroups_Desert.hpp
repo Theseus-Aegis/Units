b1dffaa9d897f5219f85202720fa3f7255fd9b2e
Mike-MF <TyroneMF@hotmail.com>
2023-07-04 00:33:56 +0100

    BAF Groups

--- addons/baf/CfgGroups_Desert.hpp
+++ addons/baf/CfgGroups_Desert.hpp
@@ -6,76 +6,25 @@ class TACU_BAF_G_Desert {
         side = 1;
         faction = "TACU_BAF_B";
         icon = "\a3\ui_f\data\map\markers\nato\n_inf.paa";
-        class unit0 {
-            vehicle = "TACU_BAF_U_B_Desert_TL";
-            side = 1;
-            rank = "SERGEANT";
-            position[] = {0, 0, 0};
-        };
-        class unit1: unit0 {
-            vehicle = "TACU_BAF_U_B_Desert_Scout";
-            rank = "PRIVATE";
-            position[] = {5, -5, 0};
-        };
+        MACRO_UNIT0(TACU_BAF_U_B_Desert_TL,1);
+        MACRO_UNIT1(TACU_BAF_U_B_Desert_Scout,1);
     };
     class TACU_BAF_G_Desert_Fireteam: TACU_BAF_G_Desert_Patrol {
         name = "Fireteam";
-        class unit0 {
-            vehicle = "TACU_BAF_U_B_Desert_TL";
-            side = 1;
-            rank = "SERGEANT";
-            position[] = {0, 0, 0};
-        };
-        class unit1: unit0 {
-            vehicle = "TACU_BAF_U_B_Desert_Medic";
-            rank = "PRIVATE";
-            position[] = {5, -5, 0};
-        };
-        class unit2: unit1 {
-            vehicle = "TACU_BAF_U_B_Desert_Scout";
-            position[] = {-5, -5, 0};
-        };
-        class unit3: unit1 {
-            vehicle = "TACU_BAF_U_B_Desert_Marksman";
-            position[] = {10, -10, 0};
-        };
+        MACRO_UNIT0(TACU_BAF_U_B_Desert_TL,1);
+        MACRO_UNIT1(TACU_BAF_U_B_Desert_Medic,1);
+        MACRO_UNIT2(TACU_BAF_U_B_Desert_Scout,1);
+        MACRO_UNIT3(TACU_BAF_U_B_Desert_Marksman,1);
     };
     class TACU_BAF_G_Desert_Squad: TACU_BAF_G_Desert_Patrol {
         name = "Squad";
-        class unit0 {
-            vehicle = "TACU_BAF_U_B_Desert_TL";
-            side = 1;
-            rank = "SERGEANT";
-            position[] = {0, 0, 0};
-        };
-        class unit1: unit0 {
-            vehicle = "TACU_BAF_U_B_Desert_Medic";
-            rank = "PRIVATE";
-            position[] = {5, -5, 0};
-        };
-        class unit2: unit1 {
-            vehicle = "TACU_BAF_U_B_Desert_Scout";
-            position[] = {-5, -5, 0};
-        };
-        class unit3: unit1 {
-            vehicle = "TACU_BAF_U_B_Desert_HeavyGunner";
-            position[] = {10, -10, 0};
-        };
-        class unit4: unit1 {
-            vehicle = "TACU_BAF_U_B_Desert_Demolitions";
-            position[] = {-10, -10, 0};
-        };
-        class unit5: unit1 {
-            vehicle = "TACU_BAF_U_B_Desert_LAT";
-            position[] = {15, -15, 0};
-        };
-        class unit6: unit1 {
-            vehicle = "TACU_BAF_U_B_Desert_Marksman";
-            position[] = {-15, -15, 0};
-        };
-        class unit7: unit1 {
-            vehicle = "TACU_BAF_U_B_Desert_Scout";
-            position[] = {20, -20, 0};
-        };
+        MACRO_UNIT0(TACU_BAF_U_B_Desert_TL,1);
+        MACRO_UNIT1(TACU_BAF_U_B_Desert_Medic,1);
+        MACRO_UNIT2(TACU_BAF_U_B_Desert_Scout,1);
+        MACRO_UNIT3(TACU_BAF_U_B_Desert_HeavyGunner,1);
+        MACRO_UNIT4(TACU_BAF_U_B_Desert_Demolitions,1);
+        MACRO_UNIT5(TACU_BAF_U_B_Desert_LAT,1);
+        MACRO_UNIT6(TACU_BAF_U_B_Desert_Marksman,1);
+        MACRO_UNIT7(TACU_BAF_U_B_Desert_Scout,1);
     };
 };
--- addons/baf/CfgGroups_Urban.hpp
+++ addons/baf/CfgGroups_Urban.hpp
@@ -6,76 +6,25 @@ class TACU_BAF_G_Urban {
         side = 1;
         faction = "TACU_BAF_B";
         icon = "\a3\ui_f\data\map\markers\nato\n_inf.paa";
-        class unit0 {
-            vehicle = "TACU_BAF_U_B_Urban_TL";
-            side = 1;
-            rank = "SERGEANT";
-            position[] = {0, 0, 0};
-        };
-        class unit1: unit0 {
-            vehicle = "TACU_BAF_U_B_Urban_Scout";
-            rank = "PRIVATE";
-            position[] = {5, -5, 0};
-        };
+        MACRO_UNIT0(TACU_BAF_U_B_Urban_TL,1);
+        MACRO_UNIT1(TACU_BAF_U_B_Urban_Scout,1);
     };
     class TACU_BAF_G_Urban_Fireteam: TACU_BAF_G_Urban_Patrol {
         name = "Fireteam";
-        class unit0 {
-            vehicle = "TACU_BAF_U_B_Urban_TL";
-            side = 1;
-            rank = "SERGEANT";
-            position[] = {0, 0, 0};
-        };
-        class unit1: unit0 {
-            vehicle = "TACU_BAF_U_B_Urban_Medic";
-            rank = "PRIVATE";
-            position[] = {5, -5, 0};
-        };
-        class unit2: unit1 {
-            vehicle = "TACU_BAF_U_B_Urban_Grenadier";
-            position[] = {-5, -5, 0};
-        };
-        class unit3: unit1 {
-            vehicle = "TACU_BAF_U_B_Urban_Marksman";
-            position[] = {10, -10, 0};
-        };
+        MACRO_UNIT0(TACU_BAF_U_B_Urban_TL,1);
+        MACRO_UNIT1(TACU_BAF_U_B_Urban_Medic,1);
+        MACRO_UNIT2(TACU_BAF_U_B_Urban_Grenadier,1);
+        MACRO_UNIT3(TACU_BAF_U_B_Urban_Marksman,1);
     };
     class TACU_BAF_G_Urban_Squad: TACU_BAF_G_Urban_Patrol {
         name = "Squad";
-        class unit0 {
-            vehicle = "TACU_BAF_U_B_Urban_TL";
-            side = 1;
-            rank = "SERGEANT";
-            position[] = {0, 0, 0};
-        };
-        class unit1: unit0 {
-            vehicle = "TACU_BAF_U_B_Urban_Medic";
-            rank = "PRIVATE";
-            position[] = {5, -5, 0};
-        };
-        class unit2: unit1 {
-            vehicle = "TACU_BAF_U_B_Urban_Grenadier";
-            position[] = {-5, -5, 0};
-        };
-        class unit3: unit1 {
-            vehicle = "TACU_BAF_U_B_Urban_HeavyGunner";
-            position[] = {10, -10, 0};
-        };
-        class unit4: unit1 {
-            vehicle = "TACU_BAF_U_B_Urban_Demolitions";
-            position[] = {-10, -10, 0};
-        };
-        class unit5: unit1 {
-            vehicle = "TACU_BAF_U_B_Urban_LAT";
-            position[] = {15, -15, 0};
-        };
-        class unit6: unit1 {
-            vehicle = "TACU_BAF_U_B_Urban_Marksman";
-            position[] = {-15, -15, 0};
-        };
-        class unit7: unit1 {
-            vehicle = "TACU_BAF_U_B_Urban_Scout";
-            position[] = {20, -20, 0};
-        };
+        MACRO_UNIT0(TACU_BAF_U_B_Urban_TL,1);
+        MACRO_UNIT1(TACU_BAF_U_B_Urban_Medic,1);
+        MACRO_UNIT2(TACU_BAF_U_B_Urban_Grenadier,1);
+        MACRO_UNIT3(TACU_BAF_U_B_Urban_HeavyGunner,1);
+        MACRO_UNIT4(TACU_BAF_U_B_Urban_Demolitions,1);
+        MACRO_UNIT5(TACU_BAF_U_B_Urban_LAT,1);
+        MACRO_UNIT6(TACU_BAF_U_B_Urban_Marksman,1);
+        MACRO_UNIT7(TACU_BAF_U_B_Urban_Scout,1);
     };
 };
