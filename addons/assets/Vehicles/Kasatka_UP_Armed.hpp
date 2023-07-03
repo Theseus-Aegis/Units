0c7b39aa317510a7b69b465cfa6319ce2bfae042
Mike-MF <TyroneMF@hotmail.com>
2023-07-03 21:48:58 +0100

    Re-add accidentally removed textures

--- addons/assets/Vehicles/Kasatka_UP_Armed.hpp
+++ addons/assets/Vehicles/Kasatka_UP_Armed.hpp
@@ -46,7 +46,12 @@ class TACU_Kasatka_Armed_Armoured_Base: B_ION_Heli_Light_02_dynamicLoadout_lxWS 
                 "TACU_African_North_O",
                 "TACU_African_North_I"
             };
-            textures[] = {QPATHTOF(data\kasatka\heli_light_02_na_grn_ext_co.paa)};
+            textures[] = {
+                QPATHTOF(data\kasatka\heli_light_02_na_grn_ext_co.paa),
+                "\a3\air_f\data\rockets_co.paa",
+                "\lxWS\air_f_lxWS\heli_light_02\data\lxws_heli_light_02_adds_ion_co.paa"
+            };
+            };
         };
         class North_African_Yellow {
             author = "GilleeDoo";
@@ -56,7 +61,12 @@ class TACU_Kasatka_Armed_Armoured_Base: B_ION_Heli_Light_02_dynamicLoadout_lxWS 
                 "TACU_African_North_O",
                 "TACU_African_North_I"
             };
-            textures[] = {QPATHTOF(data\kasatka\heli_light_02_na_ylw_ext_co.paa)};
+            textures[] = {
+                QPATHTOF(data\kasatka\heli_light_02_na_ylw_ext_co.paa),
+                "\a3\air_f\data\rockets_co.paa",
+                "\lxWS\air_f_lxWS\heli_light_02\data\lxws_heli_light_02_adds_ion_co.paa"
+            };
+            };
         };
     };
 
