0457d1f2a5096a57aa4596780d0a1f1028185ab8
Mike-MF <TyroneMF@hotmail.com>
2023-07-04 01:21:21 +0100

    Taliban

--- addons/taliban/Groups/CfgGroups_Militia_I.hpp
+++ addons/taliban/Groups/CfgGroups_Militia_I.hpp
@@ -6,79 +6,27 @@ class TACU_Taliban_Militia_Infantry_I {
         side = 2;
         faction = "TACU_Taliban_Militia_I";
         icon = "\a3\ui_f\data\map\markers\nato\n_inf.paa";
-        class unit0 {
-            vehicle = "TACU_Taliban_Militia_U_I_Rifleman_01";
-            side = 2;
-            rank = "SERGEANT";
-            position[] = {0, 0, 0};
-        };
-        class unit1: unit0 {
-            vehicle = "TACU_Taliban_Militia_U_I_Rifleman_03";
-            rank = "PRIVATE";
-            position[] = {5, -5, 0};
-        };
+        MACRO_UNIT0(TACU_Taliban_Militia_U_I_Rifleman_01,2);
+        MACRO_UNIT1(TACU_Taliban_Militia_U_I_Rifleman_03,2);
     };
 
     class TACU_Taliban_Militia_Fireteam_I: TACU_Taliban_Militia_Patrol_I {
         name = "Fireteam";
-        class unit0 {
-            vehicle = "TACU_Taliban_Militia_U_I_Leader";
-            side = 2;
-            rank = "SERGEANT";
-            position[] = {0, 0, 0};
-        };
-        class unit1: unit0 {
-            vehicle = "TACU_Taliban_Militia_U_I_Medic";
-            rank = "PRIVATE";
-            position[] = {5, -5, 0};
-        };
-        class unit2: unit1 {
-            vehicle = "TACU_Taliban_Militia_U_I_Rifleman_02";
-            position[] = {-5, -5, 0};
-
-        };
-        class unit3: unit1 {
-            vehicle = "TACU_Taliban_Militia_U_I_Machinegunner";
-            position[] = {10, -10, 0};
-        };
+        MACRO_UNIT0(TACU_Taliban_Militia_U_I_Leader,2);
+        MACRO_UNIT1(TACU_Taliban_Militia_U_I_Medic,2);
+        MACRO_UNIT2(TACU_Taliban_Militia_U_I_Rifleman_02,2);
+        MACRO_UNIT3(TACU_Taliban_Militia_U_I_Machinegunner,2);
     };
 
     class TACU_Taliban_Militia_Squad_I: TACU_Taliban_Militia_Patrol_I {
         name = "Squad";
-        class unit0 {
-            vehicle = "TACU_Taliban_Militia_U_I_Leader";
-            side = 2;
-            rank = "SERGEANT";
-            position[] = {0, 0, 0};
-        };
-        class unit1: unit0 {
-            vehicle = "TACU_Taliban_Militia_U_I_AT";
-            rank = "PRIVATE";
-            position[] = {5, -5, 0};
-        };
-        class unit2: unit1 {
-            vehicle = "TACU_Taliban_Militia_U_I_Machinegunner";
-            position[] = {-5, -5, 0};
-        };
-        class unit3: unit1 {
-            vehicle = "TACU_Taliban_Militia_U_I_Demolitions";
-            position[] = {10, -10, 0};
-        };
-        class unit4: unit1 {
-            vehicle = "TACU_Taliban_Militia_U_I_Medic";
-            position[] = {-10, -10, 0};
-        };
-        class unit5: unit1 {
-            vehicle = "TACU_Taliban_Militia_U_I_Grenadier";
-            position[] = {15, -15, 0};
-        };
-        class unit6: unit1 {
-            vehicle = "TACU_Taliban_Militia_U_I_Rifleman_03";
-            position[] = {-15, -15, 0};
-        };
-        class unit7: unit1 {
-            vehicle = "TACU_Taliban_Militia_U_I_Rifleman_01";
-            position[] = {20, -20, 0};
-        };
+        MACRO_UNIT0(TACU_Taliban_Militia_U_I_Leader,2);
+        MACRO_UNIT1(TACU_Taliban_Militia_U_I_AT,2);
+        MACRO_UNIT2(TACU_Taliban_Militia_U_I_Machinegunner,2);
+        MACRO_UNIT3(TACU_Taliban_Militia_U_I_Demolitions,2);
+        MACRO_UNIT4(TACU_Taliban_Militia_U_I_Medic,2);
+        MACRO_UNIT5(TACU_Taliban_Militia_U_I_Grenadier,2);
+        MACRO_UNIT6(TACU_Taliban_Militia_U_I_Rifleman_03,2);
+        MACRO_UNIT7(TACU_Taliban_Militia_U_I_Rifleman_01,2);
     };
 };
