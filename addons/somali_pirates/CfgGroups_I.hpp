94d02d4fe69ccf6e2c35791e940fb04e429a0deb
Mike-MF <TyroneMF@hotmail.com>
2023-07-04 01:14:35 +0100

    Somali Pirates

--- addons/somali_pirates/CfgGroups_I.hpp
+++ addons/somali_pirates/CfgGroups_I.hpp
@@ -9,81 +9,28 @@ class TACU_Somali_Pirates_I {
             side = 2;
             faction = "TACU_Somali_I";
             icon = "\a3\ui_f\data\map\markers\nato\n_inf.paa";
-            class unit0 {
-                vehicle = "TACU_Somali_Pirates_U_I_Rifleman_01";
-                side = 2;
-                rank = "SERGEANT";
-                position[] = {0, 0, 0};
-            };
-            class unit1: unit0 {
-                vehicle = "TACU_Somali_Pirates_U_I_Rifleman_02";
-                rank = "PRIVATE";
-                position[] = {5, -5, 0};
-            };
+            MACRO_UNIT0(TACU_Somali_Pirates_U_I_Rifleman_01,2);
+            MACRO_UNIT1(TACU_Somali_Pirates_U_I_Rifleman_02,2);
         };
 
         class TACU_Somali_Pirates_Fireteam_I: TACU_Somali_Pirates_Patrol_I {
             name = "Fireteam";
-            class unit0 {
-                vehicle = "TACU_Somali_Pirates_U_I_Rifleman_03";
-                side = 2;
-                rank = "SERGEANT";
-                position[] = {0, 0, 0};
-            };
-            class unit1: unit0 {
-                vehicle = "TACU_Somali_Pirates_U_I_Rifleman_04";
-                rank = "PRIVATE";
-                position[] = {5, -5, 0};
-            };
-            class unit2: unit1 {
-                vehicle = "TACU_Somali_Pirates_U_I_Autorifleman";
-                position[] = {-5, -5, 0};
-
-            };
-            class unit3: unit1 {
-                vehicle = "TACU_Somali_Pirates_U_I_AT";
-                position[] = {10, -10, 0};
-            };
+            MACRO_UNIT0(TACU_Somali_Pirates_U_I_Rifleman_03,2);
+            MACRO_UNIT1(TACU_Somali_Pirates_U_I_Rifleman_04,2);
+            MACRO_UNIT2(TACU_Somali_Pirates_U_I_Autorifleman,2);
+            MACRO_UNIT3(TACU_Somali_Pirates_U_I_AT,2);
         };
 
         class TACU_Somali_Pirates_Squad_I: TACU_Somali_Pirates_Patrol_I {
             name = "Squad";
-            class unit0 {
-                vehicle = "TACU_Somali_Pirates_U_I_Rifleman_05";
-                side = 2;
-                rank = "SERGEANT";
-                position[] = {0, 0, 0};
-            };
-            class unit1: unit0 {
-                vehicle = "TACU_Somali_Pirates_U_I_Rifleman_06";
-                rank = "PRIVATE";
-                position[] = {5, -5, 0};
-            };
-            class unit2: unit1 {
-                vehicle = "TACU_Somali_Pirates_U_I_Rifleman_04";
-                position[] = {-5, -5, 0};
-
-            };
-            class unit3: unit1 {
-                vehicle = "TACU_Somali_Pirates_U_I_Autorifleman";
-                position[] = {10, -10, 0};
-            };
-            class unit4: unit1 {
-                vehicle = "TACU_Somali_Pirates_U_I_AT";
-                position[] = {-10, -10, 0};
-            };
-            class unit5: unit1 {
-                vehicle = "TACU_Somali_Pirates_U_I_Rifleman_03";
-                position[] = {15, -15, 0};
-            };
-            class unit6: unit1 {
-                vehicle = "TACU_Somali_Pirates_U_I_Rifleman_02";
-                position[] = {-15, -15, 0};
-            };
-            class unit7: unit1 {
-                vehicle = "TACU_Somali_Pirates_U_I_Rifleman_01";
-                position[] = {20, -20, 0};
-            };
+            MACRO_UNIT0(TACU_Somali_Pirates_U_I_Rifleman_05,2);
+            MACRO_UNIT1(TACU_Somali_Pirates_U_I_Rifleman_06,2);
+            MACRO_UNIT2(TACU_Somali_Pirates_U_I_Rifleman_04,2);
+            MACRO_UNIT3(TACU_Somali_Pirates_U_I_Autorifleman,2);
+            MACRO_UNIT4(TACU_Somali_Pirates_U_I_AT,2);
+            MACRO_UNIT5(TACU_Somali_Pirates_U_I_Rifleman_03,2);
+            MACRO_UNIT6(TACU_Somali_Pirates_U_I_Rifleman_02,2);
+            MACRO_UNIT7(TACU_Somali_Pirates_U_I_Rifleman_01,2);
         };
     };
 };
