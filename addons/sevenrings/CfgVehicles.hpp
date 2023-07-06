b4d2fb548f8aa8260ad5b996ad69755d69551074
Mike-MF <TyroneMF@hotmail.com>
2023-07-06 20:14:32 +0100

    Fix includes

--- addons/sevenrings/CfgVehicles.hpp
+++ addons/sevenrings/CfgVehicles.hpp
@@ -5,13 +5,7 @@ class CfgVehicles {
     // Western Base Classes
     class B_Boat_Transport_01_F;
 
-    #include "CfgVehicles_Backpacks.hpp"
-    #include "CfgVehicles_I_Eastern.hpp"
-    #include "CfgVehicles_I_Eastern_Vehicles.hpp"
-    #include "CfgVehicles_I_Western.hpp"
-    #include "CfgVehicles_I_Western_Vehicles.hpp"
-    #include "CfgVehicles_O_Eastern.hpp"
-    #include "CfgVehicles_O_Eastern_Vehicles.hpp"
-    #include "CfgVehicles_O_Western.hpp"
-    #include "CfgVehicles_O_Western_Vehicles.hpp"
+    #include "Deprecated\backpacks.hpp"
+    #include "Deprecated\units.hpp"
+    #include "Deprecated\vehicles.hpp"
 };
