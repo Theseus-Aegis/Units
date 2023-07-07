bfff7624fa2eef1800ebd326ccd3eddd2d52651c
Mike-MF <TyroneMF@hotmail.com>
2023-07-07 18:43:46 +0100

    Assets - Fix Otokar textureSources (#97)

--- addons/assets/Vehicles/Otokar_ARMA_Armed.hpp
+++ addons/assets/Vehicles/Otokar_ARMA_Armed.hpp
@@ -14,6 +14,7 @@ class TACU_Otokar_ARMA_Armed_Base: O_APC_Wheeled_02_hmg_lxWS {
             textures[] = {
                 "lxWS\vehicles_1_f_lxws\APC_Wheeled_02\data\APC_Wheeled_02_ext_01_sfia_CO.paa",
                 "lxWS\vehicles_1_f_lxws\APC_Wheeled_02\data\APC_Wheeled_02_ext_02_sfia_CO.paa",
+                "lxWS\vehicles_1_f_lxws\APC_Wheeled_02\data\APC_Wheeled_02_adds_01_sfia_CO.paa",
                 "A3\Armor_F\Data\camonet_CSAT_HEX_Desert_CO.paa",
                 "A3\armor_f\data\cage_csat_co.paa",
                 "lxWS\vehicles_1_f_lxws\APC_Wheeled_02\data\APC_Wheeled_02_adds_02_sfia_CO.paa"
@@ -26,6 +27,7 @@ class TACU_Otokar_ARMA_Armed_Base: O_APC_Wheeled_02_hmg_lxWS {
             textures[] = {
                 "lxWS\vehicles_1_f_lxws\APC_Wheeled_02\data\APC_Wheeled_02_ext_01_black_CO.paa",
                 "lxWS\vehicles_1_f_lxws\APC_Wheeled_02\data\APC_Wheeled_02_ext_02_black_CO.paa",
+                "lxWS\vehicles_1_f_lxws\APC_Wheeled_02\data\APC_Wheeled_02_adds_01_black_CO.paa",
                 "a3\armor_f\Data\camonet_AAF_stripe_desert_CO.paa",
                 "lxws\vehicles_f_lxws\data\APC_Wheeled_01\cage_black_CO.paa",
                 "lxWS\vehicles_1_f_lxws\APC_Wheeled_02\data\APC_Wheeled_02_adds_02_black_CO.paa"
@@ -38,6 +40,7 @@ class TACU_Otokar_ARMA_Armed_Base: O_APC_Wheeled_02_hmg_lxWS {
             textures[] = {
                 "lxWS\vehicles_1_f_lxws\APC_Wheeled_02\data\APC_Wheeled_02_ext_01_nato_CO.paa",
                 "lxWS\vehicles_1_f_lxws\APC_Wheeled_02\data\APC_Wheeled_02_ext_02_nato_CO.paa",
+                "lxWS\vehicles_1_f_lxws\APC_Wheeled_02\data\APC_Wheeled_02_adds_01_nato_CO.paa",
                 "a3\Armor_F\Data\camonet_NATO_Desert_CO.paa",
                 "a3\Armor_F\Data\cage_sand_CO.paa",
                 "lxWS\vehicles_1_f_lxws\APC_Wheeled_02\data\APC_Wheeled_02_adds_02_nato_CO.paa"
@@ -50,33 +53,10 @@ class TACU_Otokar_ARMA_Armed_Base: O_APC_Wheeled_02_hmg_lxWS {
             textures[] = {
                 "lxWS\vehicles_1_f_lxws\APC_Wheeled_02\data\APC_Wheeled_02_ext_01_nato_CO.paa",
                 "lxWS\vehicles_1_f_lxws\APC_Wheeled_02\data\APC_Wheeled_02_ext_02_nato_CO.paa",
+                "lxWS\vehicles_1_f_lxws\APC_Wheeled_02\data\APC_Wheeled_02_adds_01_nato_CO.paa",
                 "lxws\vehicles_f_lxws\data\camonet_NATO_flat_desert_CO.paa",
                 "A3\armor_f\data\cage_sand_CO.paa",
                 "lxWS\vehicles_1_f_lxws\APC_Wheeled_02\data\APC_Wheeled_02_adds_02_nato_CO.paa"
-            };
-            factions[] = {};
-        };
-        class Hex {
-            displayName = "Hex";
-            author = "Rotators Collective";
-            textures[] = {
-                "a3\armor_f_beta\apc_wheeled_02\data\apc_wheeled_02_ext_01_opfor_co.paa",
-                "a3\armor_f_beta\apc_wheeled_02\data\apc_wheeled_02_ext_02_opfor_co.paa",
-                "A3\Armor_F\Data\camonet_CSAT_HEX_Desert_CO.paa",
-                "A3\armor_f\data\cage_csat_co.paa",
-                "lxWS\vehicles_1_f_lxws\APC_Wheeled_02\data\APC_Wheeled_02_adds_02_hex_CO.paa"
-            };
-            factions[] = {};
-        };
-        class GreenHex {
-            displayName = "Green Hex";
-            author = "Rotators Collective";
-            textures[] = {
-                "a3\Armor_F_Exp\APC_Wheeled_02\Data\APC_Wheeled_02_ext_01_ghex_CO.paa",
-                "a3\Armor_F_Exp\APC_Wheeled_02\Data\APC_Wheeled_02_ext_02_ghex_CO.paa",
-                "A3\Armor_F\Data\camonet_CSAT_HEX_Green_CO.paa",
-                "A3\armor_f\data\cage_csat_green_CO.paa",
-                "lxWS\vehicles_1_f_lxws\APC_Wheeled_02\data\APC_Wheeled_02_adds_02_ghex_CO.paa"
             };
             factions[] = {};
         };
@@ -86,9 +66,7 @@ class TACU_Otokar_ARMA_Armed_Base: O_APC_Wheeled_02_hmg_lxWS {
         "SFIA", 1,
         "Black", 1,
         "Sand", 1,
-        "Sand_Desert", 1,
-        "Hex", 1,
-        "GreenHex", 1
+        "Sand_Desert", 1
     };
 
     MACRO_EVENTHANDLERS;
