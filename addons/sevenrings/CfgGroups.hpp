6d8b60f686323ff0217ff4d6694928ed2bb1a96d
Mike-MF <TyroneMF@hotmail.com>
2023-07-09 19:44:34 +0100

    Eastern Groups

---
+++ addons/sevenrings/CfgGroups.hpp
@@ -0,0 +1,9 @@
+class CfgGroups {
+    class INDEP {
+        #include "CfgGroups_I.hpp"
+    };
+
+    class EAST {
+        #include "CfgGroups_O.hpp"
+    };
+};
---
+++ addons/sevenrings/CfgGroups_I.hpp
@@ -0,0 +1,3 @@
+class TACU_Seven_Rings_I {
+    name = "Seven Rings (West)";
+};
