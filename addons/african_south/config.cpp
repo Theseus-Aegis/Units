39512524128228aa7d3514e0e1880bdca6ca407b
Mike-MF <TyroneMF@hotmail.com>
2023-07-09 05:07:12 +0100

    Update config.cpp

--- addons/african_south/config.cpp
+++ addons/african_south/config.cpp
@@ -4,6 +4,13 @@ class CfgPatches {
     class ADDON {
         name = COMPONENT_NAME;
         units[] = {
+            // Backpacks
+            "TACU_African_South_B_Medic",
+            "TACU_African_South_B_Demolitions",
+            "TACU_African_South_B_MG",
+            "TACU_African_South_B_AT",
+            "TACU_African_South_B_AP",
+            // Blufor Units
             "TACU_African_South_U_B_Rifleman",
             "TACU_African_South_U_B_CQB",
             "TACU_African_South_U_B_Medic",
@@ -16,7 +23,35 @@ class CfgPatches {
             "TACU_African_South_U_B_AT",
             "TACU_African_South_U_B_AP",
             "TACU_African_South_U_B_Sniper",
-            "TACU_African_South_U_B_Spotter"
+            "TACU_African_South_U_B_Spotter",
+            // Independent Units
+            "TACU_African_South_U_I_Rifleman",
+            "TACU_African_South_U_I_CQB",
+            "TACU_African_South_U_I_Medic",
+            "TACU_African_South_U_I_Demolitions",
+            "TACU_African_South_U_I_Grenadier",
+            "TACU_African_South_U_I_Autorifleman",
+            "TACU_African_South_U_I_Machinegunner",
+            "TACU_African_South_U_I_Marksman",
+            "TACU_African_South_U_I_TL",
+            "TACU_African_South_U_I_AT",
+            "TACU_African_South_U_I_AP",
+            "TACU_African_South_U_I_Sniper",
+            "TACU_African_South_U_I_Spotter",
+            // Opfor Units
+            "TACU_African_South_U_O_Rifleman",
+            "TACU_African_South_U_O_CQB",
+            "TACU_African_South_U_O_Medic",
+            "TACU_African_South_U_O_Demolitions",
+            "TACU_African_South_U_O_Grenadier",
+            "TACU_African_South_U_O_Autorifleman",
+            "TACU_African_South_U_O_Machinegunner",
+            "TACU_African_South_U_O_Marksman",
+            "TACU_African_South_U_O_TL",
+            "TACU_African_South_U_O_AT",
+            "TACU_African_South_U_O_AP",
+            "TACU_African_South_U_O_Sniper",
+            "TACU_African_South_U_O_Spotter"
         };
         weapons[] = {
             "TACU_African_South_W_Green_XMS",
