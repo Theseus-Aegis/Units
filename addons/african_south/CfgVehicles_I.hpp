45c7e281a5b893e03dc70c248431cd122d7b81c4
Mike-MF <TyroneMF@hotmail.com>
2023-07-09 05:07:04 +0100

    Add Opfor & Independent Units

--- addons/african_south/CfgVehicles_I.hpp
+++ addons/african_south/CfgVehicles_I.hpp
@@ -2,3 +2,51 @@ class TACU_African_South_U_I_Rifleman: TACU_African_South_U_B_Rifleman {
     side = 2;
     faction = "TACU_African_South_I";
 };
+class TACU_African_South_U_I_CQB: TACU_African_South_U_B_CQB {
+    side = 2;
+    faction = "TACU_African_South_I";
+};
+class TACU_African_South_U_I_Medic: TACU_African_South_U_B_Medic {
+    side = 2;
+    faction = "TACU_African_South_I";
+};
+class TACU_African_South_U_I_Demolitions: TACU_African_South_U_B_Demolitions {
+    side = 2;
+    faction = "TACU_African_South_I";
+};
+class TACU_African_South_U_I_Grenadier: TACU_African_South_U_B_Grenadier {
+    side = 2;
+    faction = "TACU_African_South_I";
+};
+class TACU_African_South_U_I_Autorifleman: TACU_African_South_U_B_Autorifleman {
+    side = 2;
+    faction = "TACU_African_South_I";
+};
+class TACU_African_South_U_I_Machinegunner: TACU_African_South_U_B_Machinegunner {
+    side = 2;
+    faction = "TACU_African_South_I";
+};
+class TACU_African_South_U_I_Marksman: TACU_African_South_U_B_Marksman {
+    side = 2;
+    faction = "TACU_African_South_I";
+};
+class TACU_African_South_U_I_TL: TACU_African_South_U_B_TL {
+    side = 2;
+    faction = "TACU_African_South_I";
+};
+class TACU_African_South_U_I_AT: TACU_African_South_U_B_AT {
+    side = 2;
+    faction = "TACU_African_South_I";
+};
+class TACU_African_South_U_I_AP: TACU_African_South_U_B_AP {
+    side = 2;
+    faction = "TACU_African_South_I";
+};
+class TACU_African_South_U_I_Sniper: TACU_African_South_U_B_Sniper {
+    side = 2;
+    faction = "TACU_African_South_I";
+};
+class TACU_African_South_U_I_Spotter: TACU_African_South_U_B_Spotter {
+    side = 2;
+    faction = "TACU_African_South_I";
+};
--- addons/african_south/CfgVehicles_O.hpp
+++ addons/african_south/CfgVehicles_O.hpp
@@ -2,3 +2,51 @@ class TACU_African_South_U_O_Rifleman: TACU_African_South_U_B_Rifleman {
     side = 0;
     faction = "TACU_African_South_O";
 };
+class TACU_African_South_U_O_CQB: TACU_African_South_U_B_CQB {
+    side = 0;
+    faction = "TACU_African_South_O";
+};
+class TACU_African_South_U_O_Medic: TACU_African_South_U_B_Medic {
+    side = 0;
+    faction = "TACU_African_South_O";
+};
+class TACU_African_South_U_O_Demolitions: TACU_African_South_U_B_Demolitions {
+    side = 0;
+    faction = "TACU_African_South_O";
+};
+class TACU_African_South_U_O_Grenadier: TACU_African_South_U_B_Grenadier {
+    side = 0;
+    faction = "TACU_African_South_O";
+};
+class TACU_African_South_U_O_Autorifleman: TACU_African_South_U_B_Autorifleman {
+    side = 0;
+    faction = "TACU_African_South_O";
+};
+class TACU_African_South_U_O_Machinegunner: TACU_African_South_U_B_Machinegunner {
+    side = 0;
+    faction = "TACU_African_South_O";
+};
+class TACU_African_South_U_O_Marksman: TACU_African_South_U_B_Marksman {
+    side = 0;
+    faction = "TACU_African_South_O";
+};
+class TACU_African_South_U_O_TL: TACU_African_South_U_B_TL {
+    side = 0;
+    faction = "TACU_African_South_O";
+};
+class TACU_African_South_U_O_AT: TACU_African_South_U_B_AT {
+    side = 0;
+    faction = "TACU_African_South_O";
+};
+class TACU_African_South_U_O_AP: TACU_African_South_U_B_AP {
+    side = 0;
+    faction = "TACU_African_South_O";
+};
+class TACU_African_South_U_O_Sniper: TACU_African_South_U_B_Sniper {
+    side = 0;
+    faction = "TACU_African_South_O";
+};
+class TACU_African_South_U_O_Spotter: TACU_African_South_U_B_Spotter {
+    side = 0;
+    faction = "TACU_African_South_O";
+};
