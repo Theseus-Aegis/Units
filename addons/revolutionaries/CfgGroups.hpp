f023601a38eb3b87ac59e249feff7ac60f00c8fa
Mike-MF <TyroneMF@hotmail.com>
2023-07-15 22:46:28 +0100

    Cartel/Revolutionaries group naming system

--- addons/revolutionaries/CfgGroups.hpp
+++ addons/revolutionaries/CfgGroups.hpp
@@ -2,14 +2,14 @@ class CfgGroups {
     class INDEP {
         class TACU_Revolutionaries_I {
             name = "Civil Defense";
-            #include "CfgGroups_Indep_Tanoans.hpp"
+            #include "CfgGroups_I.hpp"
         };
     };
 
     class EAST {
         class TACU_Revolutionaries_O {
             name = "Revolutionaries";
-            #include "CfgGroups_Opfor_Tanoans.hpp"
+            #include "CfgGroups_O.hpp"
         };
     };
 };
