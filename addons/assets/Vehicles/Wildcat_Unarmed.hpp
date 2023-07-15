2fcd3ee9a78c37df2f25e3bdb35003c6edde8582
Mike-MF <TyroneMF@hotmail.com>
2023-07-15 21:23:02 +0100

    Wildcat Textures

--- addons/assets/Vehicles/Wildcat_Unarmed.hpp
+++ addons/assets/Vehicles/Wildcat_Unarmed.hpp
@@ -13,18 +13,19 @@ class TACU_Wildcat_Base: I_Heli_light_03_unarmed_F {
             textures[] = {"\x\tacs\addons\vehicles\data\heli_wildcat_black_co.paa"};
             factions[] = {};
         };
-        class Green {
+        class Green: Black {
             displayName = "Green";
             author = "Bohemia Interactive";
             textures[] = {"\a3\air_f_epb\heli_light_03\data\heli_light_03_base_co.paa"};
-            factions[] = {};
+        };
+        class Seven_Rings_West: Black {
+            displayName = "Seven Rings West";
+            textures[] = {QPATHTOF(data\wildcat\heli_light_03_base_srw_co.paa)};
+            factions[] = {"TACU_Seven_Rings_Western"};
         };
     };
 
-    textureList[] = {
-        "Black", 1,
-        "Green", 1
-    };
+    textureList[] = {"Black", 1, "Green", 1};
 
     MACRO_EVENTHANDLERS;
 };
