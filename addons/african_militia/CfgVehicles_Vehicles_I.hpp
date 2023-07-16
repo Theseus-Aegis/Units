a8a34142537972020818c76c11a0e1f82abb9109
Mike-MF <TyroneMF@hotmail.com>
2023-07-16 03:29:39 +0100

    Vehicles

---
+++ addons/african_militia/CfgVehicles_Vehicles_I.hpp
@@ -0,0 +1,54 @@
+class TACU_African_Militia_V_I_Offroad: TACU_African_Militia_V_O_Offroad {
+    side = 2;
+    faction = "TACU_African_Militia_I";
+    crew = "TACU_African_Militia_U_I_Repair";
+    typicalCargo[] = {"TACU_African_Militia_U_I_Repair"};
+};
+class TACU_African_Militia_V_I_Offroad_Armed: TACU_African_Militia_V_O_Offroad_Armed {
+    side = 2;
+    faction = "TACU_African_Militia_I";
+    crew = "TACU_African_Militia_U_I_Repair";
+    typicalCargo[] = {"TACU_African_Militia_U_I_Repair"};
+};
+class TACU_African_Militia_V_I_Offroad_AT: TACU_African_Militia_V_O_Offroad_AT {
+    side = 2;
+    faction = "TACU_African_Militia_I";
+    crew = "TACU_African_Militia_U_I_Repair";
+    typicalCargo[] = {"TACU_African_Militia_U_I_Repair"};
+};
+class TACU_African_Militia_V_I_Offroad_UP: TACU_African_Militia_V_O_Offroad_UP {
+    side = 2;
+    faction = "TACU_African_Militia_I";
+    crew = "TACU_African_Militia_U_I_Repair";
+    typicalCargo[] = {"TACU_African_Militia_U_I_Repair"};
+};
+class TACU_African_Militia_V_I_Offroad_UP_Armed: TACU_African_Militia_V_O_Offroad_UP_Armed {
+    side = 2;
+    faction = "TACU_African_Militia_I";
+    crew = "TACU_African_Militia_U_I_Repair";
+    typicalCargo[] = {"TACU_African_Militia_U_I_Repair"};
+};
+class TACU_African_Militia_V_I_Offroad_UP_AT: TACU_African_Militia_V_O_Offroad_UP_AT {
+    side = 2;
+    faction = "TACU_African_Militia_I";
+    crew = "TACU_African_Militia_U_I_Repair";
+    typicalCargo[] = {"TACU_African_Militia_U_I_Repair"};
+};
+class TACU_African_Militia_V_I_Truck: TACU_African_Militia_V_O_Truck {
+    side = 2;
+    faction = "TACU_African_Militia_I";
+    crew = "TACU_African_Militia_U_I_Repair";
+    typicalCargo[] = {"TACU_African_Militia_U_I_Repair"};
+};
+class TACU_African_Militia_V_I_Kamaz: TACU_African_Militia_V_O_Kamaz {
+    side = 2;
+    faction = "TACU_African_Militia_I";
+    crew = "TACU_African_Militia_U_I_Repair";
+    typicalCargo[] = {"TACU_African_Militia_U_I_Repair"};
+};
+class TACU_African_Militia_V_I_Kamaz_ZU23: TACU_African_Militia_V_O_Kamaz_ZU23 {
+    side = 2;
+    faction = "TACU_African_Militia_I";
+    crew = "TACU_African_Militia_U_I_Repair";
+    typicalCargo[] = {"TACU_African_Militia_U_I_Repair"};
+};
---
+++ addons/african_militia/CfgVehicles_Vehicles_O.hpp
@@ -0,0 +1,164 @@
+class TACU_African_Militia_V_O_Offroad: TACU_Offroad_Unarmed_Base {
+    MACRO_FACTION_VEHICLE_DEFAULTS;
+    faction = "TACU_African_Militia_O";
+    side = 0;
+    crew = "TACU_African_Militia_U_O_Repair";
+    typicalCargo[] = {"TACU_African_Militia_U_O_Repair"};
+    textureList[] = {"Guerilla_02", 1};
+    animationList[] = {
+        "HideDoor1", 0,
+        "HideDoor2", 0,
+        "HideDoor3", 0,
+        "HideBackpacks", 1,
+        "HideBumper1", 1,
+        "HideBumper2", 0,
+        "HideConstruction", 0,
+        "hidePolice", 1,
+        "HideServices", 1,
+        "BeaconsStart", 0,
+        "BeaconsServicesStart", 0
+    };
+};
+class TACU_African_Militia_V_O_Offroad_Armed: TACU_Offroad_Armed_M2_Base {
+    MACRO_FACTION_VEHICLE_DEFAULTS;
+    faction = "TACU_African_Militia_O";
+    side = 0;
+    crew = "TACU_African_Militia_U_O_Repair";
+    typicalCargo[] = {"TACU_African_Militia_U_O_Repair"};
+    textureList[] = {"Guerilla_02", 1};
+    animationList[] = {
+        "Hide_Shield", 0,
+        "Hide_Rail", 0,
+        "HideDoor1", 0,
+        "HideDoor2", 0,
+        "HideDoor3", 0,
+        "HideBackpacks", 1,
+        "HideBumper1", 1,
+        "HideBumper2", 0,
+        "HideConstruction", 0
+    };
+};
+class TACU_African_Militia_V_O_Offroad_AT: TACU_Offroad_Armed_AT_Base {
+    MACRO_FACTION_VEHICLE_DEFAULTS;
+    faction = "TACU_African_Militia_O";
+    side = 0;
+    crew = "TACU_African_Militia_U_O_Repair";
+    typicalCargo[] = {"TACU_African_Militia_U_O_Repair"};
+    textureList[] = {"Guerilla_02", 1};
+    animationList[] = {
+        "HideDoor1", 0,
+        "HideDoor2", 0,
+        "HideDoor3", 0,
+        "HideBackpacks", 1,
+        "HideBumper1", 1,
+        "HideBumper2", 0,
+        "HideConstruction", 0
+    };
+};
+
+class TACU_African_Militia_V_O_Offroad_UP: TACU_Offroad_Unarmed_UP_Base {
+    MACRO_FACTION_VEHICLE_DEFAULTS;
+    faction = "TACU_African_Militia_O";
+    side = 0;
+    crew = "TACU_African_Militia_U_O_Repair";
+    typicalCargo[] = {"TACU_African_Militia_U_O_Repair"};
+    textureList[] = {"Guerilla_02", 1};
+    animationList[] = {
+        "HideDoor1", 0,
+        "HideDoor2", 0,
+        "hideRoofLights", 0,
+        "HideBumper1", 1,
+        "HideBumper2", 1,
+        "HideBumper3", 0,
+        "HatchL_rot", 1,
+        "HatchR_rot", 1,
+        "HideSnorkel", 0,
+        "HideDoor3", 0,
+        "HideConstruction", 0,
+        "HideBackpacks", 1
+    };
+};
+
+class TACU_African_Militia_V_O_Offroad_UP_Armed: TACU_Offroad_Armed_M2_UP_Base {
+    MACRO_FACTION_VEHICLE_DEFAULTS;
+    faction = "TACU_African_Militia_O";
+    side = 0;
+    crew = "TACU_African_Militia_U_O_Repair";
+    typicalCargo[] = {"TACU_African_Militia_U_O_Repair"};
+    textureList[] = {"Guerilla_02", 1};
+    animationList[] = {
+        "HideDoor1", 0,
+        "HideDoor2", 0,
+        "hideRoofLights", 0,
+        "HideBumper1", 1,
+        "HideBumper2", 1,
+        "HideBumper3", 0,
+        "HatchL_rot", 1,
+        "HatchR_rot", 1,
+        "HideSnorkel", 0,
+        "Hide_Shield", 0,
+        "Hide_Rail", 0,
+        "HideDoor3", 0,
+        "HideBackpacks", 1,
+        "HideConstruction", 0
+    };
+};
+
+class TACU_African_Militia_V_O_Offroad_UP_AT: TACU_Offroad_Armed_AT_UP_Base {
+    MACRO_FACTION_VEHICLE_DEFAULTS;
+    faction = "TACU_African_Militia_O";
+    side = 0;
+    crew = "TACU_African_Militia_U_O_Repair";
+    typicalCargo[] = {"TACU_African_Militia_U_O_Repair"};
+    textureList[] = {"Guerilla_02", 1};
+    animationList[] = {
+        "HideDoor1", 0,
+        "HideDoor2", 0,
+        "hideRoofLights", 0,
+        "HideBumper1", 1,
+        "HideBumper2", 1,
+        "HideBumper3", 0,
+        "HatchL_rot", 1,
+        "HatchR_rot", 1,
+        "HideSnorkel", 0,
+        "HideDoor3", 0,
+        "HideBackpacks", 1,
+        "HideConstruction", 0
+    };
+};
+
+class TACU_African_Militia_V_O_Truck: TACU_Truck_Base {
+    MACRO_FACTION_VEHICLE_DEFAULTS;
+    faction = "TACU_African_Militia_O";
+    side = 0;
+    crew = "TACU_African_Militia_U_O_Repair";
+    typicalCargo[] = {"TACU_African_Militia_U_O_Repair"};
+    textureList[] = {"Guerilla_01", 1};
+};
+
+class TACU_African_Militia_V_O_Kamaz: TACU_Kamaz_Base {
+    MACRO_FACTION_VEHICLE_DEFAULTS;
+    faction = "TACU_African_Militia_O";
+    side = 0;
+    crew = "TACU_African_Militia_U_O_Repair";
+    typicalCargo[] = {"TACU_African_Militia_U_O_Repair"};
+    textureList[] = {"Brown", 1};
+};
+
+class TACU_African_Militia_V_O_Kamaz_ZU23: TACU_Kamaz_ZU23_Base {
+    MACRO_FACTION_VEHICLE_DEFAULTS;
+    faction = "TACU_African_Militia_O";
+    side = 0;
+    crew = "TACU_African_Militia_U_O_Repair";
+    typicalCargo[] = {"TACU_African_Militia_U_O_Repair"};
+    textureList[] = {"SFIA", 1};
+    animationList[] = {
+        "shield_hide", 0,
+        "hideCrates", 0,
+        "hideSpareWheel", 0,
+        "hideRoofRack", 1,
+        "hideBeacon_1", 0,
+        "hideBumper", 0,
+        "hideWindowProtector", 0
+    };
+};
