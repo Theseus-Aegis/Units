af3fd86fe378145cade3c0cfac77e18b78782939
Mike-MF <TyroneMF@hotmail.com>
2023-07-23 18:32:08 +0100

    African Militia

---
+++ addons/african_militia/subconfig_cup/script_component.hpp
@@ -0,0 +1,4 @@
+#define COMPONENT african_militia_cup
+#define COMPONENT_BEAUTIFIED African Militia CUP
+#include "\x\tacu\addons\main\script_mod.hpp"
+#include "\x\tacu\addons\main\script_macros.hpp"
