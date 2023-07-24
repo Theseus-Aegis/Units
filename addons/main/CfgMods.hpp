9218b082af00cfa328b2bd617ee0cf1e23746df7
Mike-MF <TyroneMF@hotmail.com>
2023-07-24 15:13:33 +0100

    Rename to Theseus Units (#116)
    Co-authored-by: JoramD <j.davids@hotmail.nl>

--- addons/main/CfgMods.hpp
+++ addons/main/CfgMods.hpp
@@ -1,15 +1,15 @@
 class CfgMods {
     class Mod_Base;
     class PREFIX: Mod_Base {
-        dir = "@tac_units";
-        name = "Theseus Inc. Units";
+        dir = "@theseus_units";
+        name = "Theseus Units";
         picture = QPATHTOF(UI\logo_tacu_ca.paa);
         logo = QPATHTOF(UI\logo_tacu_small_ca.paa);
         logoOver = QPATHTOF(UI\logo_tacu_small_ca.paa);
         logoSmall = QPATHTOF(UI\logo_tacu_small_ca.paa);
         hidePicture = "true";
         hideName = "true";
-        actionName = "Website";
+        actionName = "Github";
         action = CSTRING(URL);
     };
 };
