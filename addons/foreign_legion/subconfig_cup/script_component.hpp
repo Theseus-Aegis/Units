3e6e14589052fb8cf56ff9ff5121f0eb31883c8a
Mike-MF <TyroneMF@hotmail.com>
2023-07-25 17:15:50 +0100

    push

---
+++ addons/foreign_legion/subconfig_cup/script_component.hpp
@@ -0,0 +1,4 @@
+#define COMPONENT foreign_legion_cup
+#define COMPONENT_BEAUTIFIED Foreign Legion CUP
+#include "\x\tacu\addons\main\script_mod.hpp"
+#include "\x\tacu\addons\main\script_macros.hpp"
