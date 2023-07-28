af859588061a7fd6adffc600dfa6bdea1ea8baef
Mike-MF <TyroneMF@hotmail.com>
2023-07-28 16:42:39 +0100

    Backpacks

--- addons/proxy_china/config.cpp
+++ addons/proxy_china/config.cpp
@@ -3,7 +3,22 @@
 class CfgPatches {
     class ADDON {
         name = COMPONENT_NAME;
-        units[] = {};
+        units[] = {
+            // Backpacks
+            "TACU_Proxy_China_B_Demolitions",
+            "TACU_Proxy_China_B_Engineer",
+            "TACU_Proxy_China_B_Autorifleman",
+            "TACU_Proxy_China_B_Machinegunner",
+            "TACU_Proxy_China_B_Medic",
+            "TACU_Proxy_China_B_AT",
+            "TACU_Proxy_China_B_AP"
+            // Units - Opfor
+            // Vehicles - Opfor
+            // Turrets - Opfor
+            // Units - Independent
+            // Vehicles - Independent
+            // Turrets - Independent
+        };
         weapons[] = {};
         requiredVersion = REQUIRED_VERSION;
         requiredAddons[] = {"tacu_main", "tacu_assets"};
