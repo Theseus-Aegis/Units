30a41a6fddb6479887adb368a64b2a25104b9dcf
Mike-MF <TyroneMF@hotmail.com>
2023-07-28 20:38:38 +0100

    Alter Vest, remove flash hiders

--- addons/sevenrings/Western/CfgWeapons_Western.hpp
+++ addons/sevenrings/Western/CfgWeapons_Western.hpp
@@ -2,7 +2,6 @@ class TACU_Seven_Rings_Western_W_MSBS: arifle_MSBS65_F {
     MACRO_WEAPON_DEFAULTS;
     class LinkedItems {
         EQUIP_OPTIC(optic_ico_01_f);
-        EQUIP_MUZZLE(ace_muzzle_mzls_h);
         EQUIP_POINTER(acc_pointer_ir);
     };
 };
@@ -11,7 +10,6 @@ class TACU_Seven_Rings_Western_W_MSBS_GL: arifle_MSBS65_GL_F {
     MACRO_WEAPON_DEFAULTS;
     class LinkedItems {
         EQUIP_OPTIC(optic_ico_01_f);
-        EQUIP_MUZZLE(ace_muzzle_mzls_h);
         EQUIP_POINTER(acc_pointer_ir);
     };
 };
@@ -20,7 +18,6 @@ class TACU_Seven_Rings_Western_W_MSBS_MR: arifle_MSBS65_Mark_F {
     MACRO_WEAPON_DEFAULTS;
     class LinkedItems {
         EQUIP_OPTIC(optic_dms);
-        EQUIP_MUZZLE(ace_muzzle_mzls_h);
         EQUIP_POINTER(acc_pointer_ir);
     };
 };
@@ -29,7 +26,6 @@ class TACU_Seven_Rings_Western_W_MSBS_SG: arifle_MSBS65_UBS_F {
     MACRO_WEAPON_DEFAULTS;
     class LinkedItems {
         EQUIP_OPTIC(optic_hamr);
-        EQUIP_MUZZLE(ace_muzzle_mzls_h);
         EQUIP_POINTER(acc_pointer_ir);
     };
 };
@@ -38,7 +34,6 @@ class TACU_Seven_Rings_Western_W_Stoner99: LMG_Mk200_black_F {
     MACRO_WEAPON_DEFAULTS;
     class LinkedItems {
         EQUIP_OPTIC(optic_mrco);
-        EQUIP_MUZZLE(ace_muzzle_mzls_h);
         EQUIP_POINTER(acc_pointer_ir);
     };
 };
@@ -47,7 +42,6 @@ class TACU_Seven_Rings_Western_W_AK15K: arifle_AK12U_F {
     MACRO_WEAPON_DEFAULTS;
     class LinkedItems {
         EQUIP_OPTIC(optic_holosight_blk_f);
-        EQUIP_MUZZLE(ace_muzzle_mzls_b);
         EQUIP_POINTER(acc_pointer_ir);
     };
 };
@@ -56,7 +50,6 @@ class TACU_Seven_Rings_Western_W_AK15: arifle_AK12_F {
     MACRO_WEAPON_DEFAULTS;
     class LinkedItems {
         EQUIP_OPTIC(optic_holosight_blk_f);
-        EQUIP_MUZZLE(ace_muzzle_mzls_b);
         EQUIP_POINTER(acc_pointer_ir);
     };
 };
@@ -65,7 +58,6 @@ class TACU_Seven_Rings_Western_W_LWMMG: MMG_02_black_F {
     MACRO_WEAPON_DEFAULTS;
     class LinkedItems {
         EQUIP_OPTIC(optic_holosight_blk_f);
-        EQUIP_MUZZLE(ace_muzzle_mzls_338);
         EQUIP_POINTER(acc_pointer_ir);
     };
 };
@@ -74,7 +66,6 @@ class TACU_Seven_Rings_Western_W_Noreen: srifle_DMR_02_F {
     MACRO_WEAPON_DEFAULTS;
     class LinkedItems {
         EQUIP_OPTIC(optic_khs_blk);
-        EQUIP_MUZZLE(ace_muzzle_mzls_338);
         EQUIP_POINTER(acc_pointer_ir);
     };
 };
