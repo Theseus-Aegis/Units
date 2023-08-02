86c897b1bc1128ad07b438b9f7bde0b789d34df6
Mike-MF <TyroneMF@hotmail.com>
2023-08-02 16:36:55 +0100

    requiredAddons

--- addons/faction_overwrites/subconfig_cup/config.cpp
+++ addons/faction_overwrites/subconfig_cup/config.cpp
@@ -6,7 +6,11 @@ class CfgPatches {
         units[] = {};
         weapons[] = {};
         requiredVersion = REQUIRED_VERSION;
-        requiredAddons[] = {"tacu_main", "CUP_Creatures_People_LoadOrder"};
+        requiredAddons[] = {
+            "tacu_main",
+            "tacu_cup_ammunition",
+            "CUP_Creatures_People_LoadOrder"
+        };
         author = ECSTRING(main,Author);
         authors[] = {"Mike"};
         VERSION_CONFIG;
