f16fc56783684662c2b930187e259323ac77404a
Mike-MF <TyroneMF@hotmail.com>
2023-09-10 00:16:46 +0100

    Revolutionaries - Vehicle Previews

--- addons/revolutionaries/CfgVehicles_Vehicles.hpp
+++ addons/revolutionaries/CfgVehicles_Vehicles.hpp
@@ -12,6 +12,7 @@ class TACU_Revolutionaries_V_I_Offroad: TACU_Offroad_Unarmed_Base {
         "Blue", 0.33,
         "Black", 0.33
     };
+    EDITORPREVIEW(TACU_Revolutionaries_V_I_Offroad);
 };
 
 class TACU_Revolutionaries_V_I_Hatchback: C_Hatchback_01_F {
@@ -22,6 +23,7 @@ class TACU_Revolutionaries_V_I_Hatchback: C_Hatchback_01_F {
     crew = "TACU_Revolutionaries_U_I_Tanoan_Pistoleer02";
     typicalCargo[] = {"TACU_Revolutionaries_U_I_Tanoan_Pistoleer02"};
     editorSubcategory = "TACU_Revolutionaries_EdSubCat_Cars_Tanoa";
+    EDITORPREVIEW(TACU_Revolutionaries_V_I_Hatchback);
 };
 
 // Tanoan Vehicles - Opfor
@@ -33,6 +35,7 @@ class TACU_Revolutionaries_V_O_Hatchback: C_Hatchback_01_F {
     displayName = "Hatchback";
     crew = "TACU_Revolutionaries_U_O_Tanoan_Pistoleer01";
     typicalCargo[] = {"TACU_Revolutionaries_U_O_Tanoan_Pistoleer01"};
+    EDITORPREVIEW(TACU_Revolutionaries_V_O_Hatchback);
 };
 
 class TACU_Revolutionaries_V_O_KamAZ: TACU_Kamaz_Base {
@@ -42,8 +45,8 @@ class TACU_Revolutionaries_V_O_KamAZ: TACU_Kamaz_Base {
     side = 0;
     crew = "TACU_Revolutionaries_U_O_Tanoan_Pistoleer02";
     typicalCargo[] = {"TACU_Revolutionaries_U_O_Tanoan_Pistoleer02"};
-    textureList[] = {"Orange", 0.5, "Blue", 0.5
-    };
+    textureList[] = {"Orange", 0.5, "Blue", 0.5};
+    EDITORPREVIEW(TACU_Revolutionaries_V_O_KamAZ);
 };
 
 class TACU_Revolutionaries_V_O_Offroad: TACU_Offroad_Covered_Base {
@@ -54,6 +57,7 @@ class TACU_Revolutionaries_V_O_Offroad: TACU_Offroad_Covered_Base {
     crew = "TACU_Revolutionaries_U_O_Tanoan_Pistoleer01";
     typicalCargo[] = {"TACU_Revolutionaries_U_O_Tanoan_Pistoleer01"};
     textureList[] = {"Black", 1};
+    EDITORPREVIEW(TACU_Revolutionaries_V_O_Offroad);
 };
 
 class TACU_Revolutionaries_V_O_Van: TACU_Cargo_Van_Base {
@@ -68,6 +72,7 @@ class TACU_Revolutionaries_V_O_Van: TACU_Cargo_Van_Base {
         "Red", 0.33,
         "Green", 0.33
     };
+    EDITORPREVIEW(TACU_Revolutionaries_V_O_Van);
 };
 
 class TACU_Revolutionaries_V_O_Jeep_LMG: TACU_Jeep_Wrangler_LMG_Base {
@@ -78,4 +83,5 @@ class TACU_Revolutionaries_V_O_Jeep_LMG: TACU_Jeep_Wrangler_LMG_Base {
     crew = "TACU_Revolutionaries_U_O_Tanoan_Pistoleer02";
     typicalCargo[] = {"TACU_Revolutionaries_U_O_Tanoan_Pistoleer02"};
     textureList[] = {"Green", 1};
+    EDITORPREVIEW(TACU_Revolutionaries_V_O_Jeep_LMG);
 };
