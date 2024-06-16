1a8b034a2687410b72ac88c101637d7a353a6d2b
Mike-MF <TyroneMF@hotmail.com>
2024-06-16 01:46:20 +0100

    Taliban

---
+++ addons/taliban/subconfig_rf/CfgVehicles.hpp
@@ -0,0 +1,8 @@
+class CfgVehicles {
+    class O_G_Pickup_rf;
+    class B_Pickup_mmg_rf;
+    class O_G_Pickup_hmg_rf;
+
+    #include "CfgVehicles_Veterans.hpp"
+    #include "CfgVehicles_Militia.hpp"
+};
