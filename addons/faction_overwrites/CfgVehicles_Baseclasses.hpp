b7e3533e7632f17f26ad29ee6d7446e92f671a19
Mike-MF <TyroneMF@hotmail.com>
2024-07-30 03:22:35 +0100

    Fixes for HEMTT 1.12.4 (#152)

--- addons/faction_overwrites/CfgVehicles_Baseclasses.hpp
+++ addons/faction_overwrites/CfgVehicles_Baseclasses.hpp
@@ -1,12 +1,10 @@
 // Base classes - Vanilla
 class B_Soldier_F;
 class I_C_Soldier_base_F;
-class I_G_Soldier_base_F;
 class I_L_Soldier_Base_F;
 class I_Soldier_02_F;
 class I_Soldier_03_F;
 class I_Soldier_04_F;
-class I_Soldier_base_F;
 class I_Soldier_diver_base_F;
 class I_Soldier_sniper_base_F;
 class O_R_Soldier_Base_F;
@@ -23,7 +21,6 @@ class B_Carryall_oli;
 class B_Carryall_oucamo;
 class B_FieldPack_blk;
 class B_FieldPack_cbr;
-class B_FieldPack_cbr_Ammo;
 class B_FieldPack_khk;
 class B_FieldPack_oli;
 class B_Kitbag_cbr;
