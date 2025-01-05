ddafbe11ab800cc1ddfeba6442f87775a0d54f9c
Mike-MF <TyroneMF@hotmail.com>
2025-01-05 14:56:05 +0000

    Structure Chernobog vehicles better

---
+++ addons/chernobog/CfgVehicles_Vehicles_Children.hpp
@@ -0,0 +1,92 @@
+// Offroad
+class TACU_Chernobog_Children_V_I_Offroad: TACU_Offroad_Unarmed_Base {
+    MACRO_FACTION_VEHICLE_DEFAULTS;
+    faction = "TACU_Chernobog";
+    editorSubcategory = "TACU_Chernobog_Children_Cars";
+    side = 2;
+    crew = "TACU_Chernobog_Children_U_I_Rifleman_01";
+    typicalCargo[] = {"TACU_Chernobog_Children_U_I_Rifleman_01"};
+    textureList[] = {
+        "Guerilla_01", 0.14,
+        "Guerilla_04", 0.14,
+        "Guerilla_05", 0.14,
+        "Guerilla_07", 0.14,
+        "Guerilla_08", 0.14,
+        "Guerilla_09", 0.14,
+        "Guerilla_12", 0.14
+    };
+    EDITORPREVIEW(TACU_Chernobog_Children_V_I_Offroad);
+};
+// HMG Offroad
+class TACU_Chernobog_Children_V_I_Offroad_HMG: TACU_Offroad_Armed_M2_Base {
+    MACRO_FACTION_VEHICLE_DEFAULTS;
+    faction = "TACU_Chernobog";
+    editorSubcategory = "TACU_Chernobog_Children_Cars";
+    side = 2;
+    crew = "TACU_Chernobog_Chosen_U_I_Rifleman";
+    typicalCargo[] = {"TACU_Chernobog_Chosen_U_I_Rifleman"};
+    textureList[] = {
+        "Guerilla_01", 0.14,
+        "Guerilla_04", 0.14,
+        "Guerilla_05", 0.14,
+        "Guerilla_07", 0.14,
+        "Guerilla_08", 0.14,
+        "Guerilla_09", 0.14,
+        "Guerilla_12", 0.14
+    };
+    EDITORPREVIEW(TACU_Chernobog_Children_V_I_Offroad_HMG);
+};
+class TACU_Chernobog_Children_V_I_UAZ: TACU_UAZ_Open_Base {
+    MACRO_FACTION_VEHICLE_DEFAULTS;
+    faction = "TACU_Chernobog";
+    editorSubcategory = "TACU_Chernobog_Children_Cars";
+    side = 2;
+    crew = "TACU_Chernobog_Children_U_I_Rifleman_01";
+    typicalCargo[] = {"TACU_Chernobog_Children_U_I_Rifleman_01"};
+    //EDITORPREVIEW(TACU_Chernobog_Children_V_I_UAZ);
+};
+class TACU_Chernobog_Children_V_I_UAZ_DSHKM: TACU_UAZ_DSHKM_Base {
+    MACRO_FACTION_VEHICLE_DEFAULTS;
+    faction = "TACU_Chernobog";
+    editorSubcategory = "TACU_Chernobog_Children_Cars";
+    side = 2;
+    crew = "TACU_Chernobog_Children_U_I_Rifleman_01";
+    typicalCargo[] = {"TACU_Chernobog_Children_U_I_Rifleman_01"};
+    //EDITORPREVIEW(TACU_Chernobog_Children_V_I_UAZ_DSHKM);
+};
+class TACU_Chernobog_Children_V_I_Skoda: TACU_S1203_Base {
+    MACRO_FACTION_VEHICLE_DEFAULTS;
+    faction = "TACU_Chernobog";
+    editorSubcategory = "TACU_Chernobog_Children_Cars";
+    side = 2;
+    crew = "TACU_Chernobog_Children_U_I_Rifleman_01";
+    typicalCargo[] = {"TACU_Chernobog_Children_U_I_Rifleman_01"};
+    //EDITORPREVIEW(TACU_Chernobog_Children_V_I_Skoda);
+};
+class TACU_Chernobog_Children_V_I_Ural: TACU_Ural_Base {
+    MACRO_FACTION_VEHICLE_DEFAULTS;
+    faction = "TACU_Chernobog";
+    editorSubcategory = "TACU_Chernobog_Children_Cars";
+    side = 2;
+    crew = "TACU_Chernobog_Children_U_I_Rifleman_01";
+    typicalCargo[] = {"TACU_Chernobog_Children_U_I_Rifleman_01"};
+    //EDITORPREVIEW(TACU_Chernobog_Children_V_I_Ural);
+};
+class TACU_Chernobog_Children_V_I_Hilux: TACU_Hilux_Base {
+    MACRO_FACTION_VEHICLE_DEFAULTS;
+    faction = "TACU_Chernobog";
+    editorSubcategory = "TACU_Chernobog_Children_Cars";
+    side = 2;
+    crew = "TACU_Chernobog_Children_U_I_Rifleman_01";
+    typicalCargo[] = {"TACU_Chernobog_Children_U_I_Rifleman_01"};
+    //EDITORPREVIEW(TACU_Chernobog_Children_V_I_Hilux);
+};
+class TACU_Chernobog_Children_V_I_Hilux_Armored: TACU_Hilux_Armored_Base {
+    MACRO_FACTION_VEHICLE_DEFAULTS;
+    faction = "TACU_Chernobog";
+    editorSubcategory = "TACU_Chernobog_Children_Cars";
+    side = 2;
+    crew = "TACU_Chernobog_Children_U_I_Rifleman_01";
+    typicalCargo[] = {"TACU_Chernobog_Children_U_I_Rifleman_01"};
+    //EDITORPREVIEW(TACU_Chernobog_Children_V_I_Hilux_Armored);
+};
---
+++ addons/chernobog/CfgVehicles_Vehicles_Chosen.hpp
@@ -0,0 +1,86 @@
+class TACU_Chernobog_Chosen_V_I_Offroad: TACU_Chernobog_Children_V_I_Offroad {
+    editorSubcategory = "TACU_Chernobog_Chosen_Cars";
+    crew = "TACU_Chernobog_Chosen_U_I_Rifleman";
+    typicalCargo[] = {"TACU_Chernobog_Chosen_U_I_Rifleman"};
+};
+class TACU_Chernobog_Chosen_V_I_Offroad_HMG: TACU_Chernobog_Children_V_I_Offroad_HMG {
+    editorSubcategory = "TACU_Chernobog_Chosen_Cars";
+    crew = "TACU_Chernobog_Chosen_U_I_Rifleman";
+    typicalCargo[] = {"TACU_Chernobog_Chosen_U_I_Rifleman"};
+};
+// UP Armoured Offroad
+class TACU_Chernobog_Chosen_V_I_Offroad_UP: TACU_Offroad_Unarmed_UP_Base {
+    MACRO_FACTION_VEHICLE_DEFAULTS;
+    faction = "TACU_Chernobog";
+    editorSubcategory = "TACU_Chernobog_Chosen_Cars";
+    side = 2;
+    crew = "TACU_Chernobog_Chosen_U_I_Rifleman";
+    typicalCargo[] = {"TACU_Chernobog_Chosen_U_I_Rifleman"};
+    textureList[] = {
+        "Guerilla_01", 0.14,
+        "Guerilla_04", 0.14,
+        "Guerilla_05", 0.14,
+        "Guerilla_07", 0.14,
+        "Guerilla_08", 0.14,
+        "Guerilla_09", 0.14,
+        "Guerilla_12", 0.14
+    };
+    EDITORPREVIEW(TACU_Chernobog_Chosen_V_I_Offroad_UP);
+};
+
+
+// UP Armoured Offroad HMG
+class TACU_Chernobog_Chosen_V_I_Offroad_HMG_UP: TACU_Offroad_Armed_M2_UP_Base {
+    MACRO_FACTION_VEHICLE_DEFAULTS;
+    faction = "TACU_Chernobog";
+    editorSubcategory = "TACU_Chernobog_Chosen_Cars";
+    side = 2;
+    crew = "TACU_Chernobog_Chosen_U_I_Rifleman";
+    typicalCargo[] = {"TACU_Chernobog_Chosen_U_I_Rifleman"};
+    textureList[] = {
+        "Guerilla_01", 0.14,
+        "Guerilla_04", 0.14,
+        "Guerilla_05", 0.14,
+        "Guerilla_07", 0.14,
+        "Guerilla_08", 0.14,
+        "Guerilla_09", 0.14,
+        "Guerilla_12", 0.14
+    };
+    EDITORPREVIEW(TACU_Chernobog_Chosen_V_I_Offroad_HMG_UP);
+};
+class TACU_Chernobog_Chosen_V_I_UAZ: TACU_Chernobog_Children_V_I_UAZ {
+    editorSubcategory = "TACU_Chernobog_Chosen_Cars";
+    crew = "TACU_Chernobog_Chosen_U_I_Rifleman";
+    typicalCargo[] = {"TACU_Chernobog_Chosen_U_I_Rifleman"};
+};
+class TACU_Chernobog_Chosen_V_I_UAZ_DSHKM: TACU_Chernobog_Children_V_I_UAZ_DSHKM {
+    editorSubcategory = "TACU_Chernobog_Chosen_Cars";
+    crew = "TACU_Chernobog_Chosen_U_I_Rifleman";
+    typicalCargo[] = {"TACU_Chernobog_Chosen_U_I_Rifleman"};
+};
+class TACU_Chernobog_Chosen_V_I_Skoda: TACU_Chernobog_Children_V_I_Skoda {
+    editorSubcategory = "TACU_Chernobog_Chosen_Cars";
+    crew = "TACU_Chernobog_Chosen_U_I_Rifleman";
+    typicalCargo[] = {"TACU_Chernobog_Chosen_U_I_Rifleman"};
+};
+class TACU_Chernobog_Chosen_V_I_Ural: TACU_Chernobog_Children_V_I_Ural {
+    editorSubcategory = "TACU_Chernobog_Chosen_Cars";
+    crew = "TACU_Chernobog_Chosen_U_I_Rifleman";
+    typicalCargo[] = {"TACU_Chernobog_Chosen_U_I_Rifleman"};
+};
+class TACU_Chernobog_Chosen_V_I_Hilux: TACU_Chernobog_Children_V_I_Hilux {
+    editorSubcategory = "TACU_Chernobog_Chosen_Cars";
+    crew = "TACU_Chernobog_Chosen_U_I_Rifleman";
+    typicalCargo[] = {"TACU_Chernobog_Chosen_U_I_Rifleman"};
+};
+class TACU_Chernobog_Chosen_V_I_Hilux_Armored: TACU_Chernobog_Children_V_I_Hilux_Armored {
+    editorSubcategory = "TACU_Chernobog_Chosen_Cars";
+    crew = "TACU_Chernobog_Chosen_U_I_Rifleman";
+    typicalCargo[] = {"TACU_Chernobog_Chosen_U_I_Rifleman"};
+};
+class TACU_Chernobog_Chosen_V_I_Hilux_BMP1: TACU_Hilux_BMP1_Base {
+    editorSubcategory = "TACU_Chernobog_Chosen_Cars";
+    crew = "TACU_Chernobog_Chosen_U_I_Rifleman";
+    typicalCargo[] = {"TACU_Chernobog_Chosen_U_I_Rifleman"};
+    //EDITORPREVIEW(TACU_Chernobog_Chosen_V_I_Hilux_BMP1);
+};
